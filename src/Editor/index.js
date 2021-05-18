d0ba24fe81546484455119ddb2337a6c313e2e06
Vinay Chandran <vinaychandranvs@gmail.com>
2021-05-18 13:33:07 +0530

    Use tailwind typography

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -2,13 +2,26 @@ import React, { forwardRef } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Typography from "@tiptap/extension-typography";
+import Placeholder from "@tiptap/extension-placeholder";
 import SlashCommands from "./CustomExtensions/SlashCommands/ExtensionConfig";
 
 const Tiptap = (props, ref) => {
 	const editor = useEditor({
-		extensions: props.extensions || [StarterKit, Typography, SlashCommands],
-		content: "<p>Hello World! 🌎️</p>",
+		extensions: props.extensions || [
+			StarterKit,
+			Typography,
+			Placeholder.configure({
+				placeholder: "Type '/' to choose a block type",
+			}),
+			SlashCommands,
+		],
+		content: null,
 		injectCSS: false,
+		editorProps: {
+			attributes: {
+				class: "prose m-5 focus:outline-none",
+			},
+		},
 	});
 
 	/* Make editor object available to the parent */
