c33cce221f96af3c73619830d1bdfd232938b2a0
Vinay Chandran <vinaychandranvs@gmail.com>
2021-05-19 14:25:45 +0530

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   settings: {
     react: {
       version: "detect",
--- src/Editor/CustomExtensions/SlashCommands/Commands.js
+++ src/Editor/CustomExtensions/SlashCommands/Commands.js
@@ -2,24 +2,24 @@ import { Extension } from "@tiptap/core";
 import Suggestion from "@tiptap/suggestion";
 
 export default Extension.create({
-	name: "mention",
+  name: "mention",
 
-	defaultOptions: {
-		suggestion: {
-			char: "/",
-			startOfLine: false,
-			command: ({ editor, range, props }) => {
-				props.command({ editor, range });
-			},
-		},
-	},
+  defaultOptions: {
+    suggestion: {
+      char: "/",
+      startOfLine: false,
+      command: ({ editor, range, props }) => {
+        props.command({ editor, range });
+      },
+    },
+  },
 
-	addProseMirrorPlugins() {
-		return [
-			Suggestion({
-				editor: this.editor,
-				...this.options.suggestion,
-			}),
-		];
-	},
+  addProseMirrorPlugins() {
+    return [
+      Suggestion({
+        editor: this.editor,
+        ...this.options.suggestion,
+      }),
+    ];
+  },
 });
