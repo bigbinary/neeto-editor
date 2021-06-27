fbd6469250ccea3bd8cec36574eacb40acaeadff
Edwin Babu <edwinbbu@gmail.com>
2021-06-27 10:24:38 +0530

    Removed unused yarn packages

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const PeerDepsExternalsPlugin = require("peer-deps-externals-webpack-plugin");
+
+module.exports = {
+  entry: "./lib/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(sass|css|scss)$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          "postcss-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  output: {
+    path: __dirname,
+    filename: "index.js",
+    library: "neetoui-editor",
+    libraryTarget: "umd",
+  },
+  plugins: [new PeerDepsExternalsPlugin()],
+};
