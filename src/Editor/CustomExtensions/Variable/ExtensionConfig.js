6f2f108abc840e8bd722084539f457c5fee3a80a
Labeeb Latheef <labeebklatif@gmail.com>
2021-10-28 23:45:38 +0530

    removed unused variables

--- src/Editor/CustomExtensions/Variable/ExtensionConfig.js
+++ src/Editor/CustomExtensions/Variable/ExtensionConfig.js
@@ -88,10 +88,7 @@ const Variable = Node.create({
     return {
       setVariable:
         (attributes) =>
-        ({ chain, state, editor }) => {
-          const { selection } = state;
-          const range = { from: selection.$anchor.pos };
-
+        ({ chain }) => {
           chain()
             .focus()
             .insertContent([
