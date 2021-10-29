63bcdecda02217dfe676aac21a95ab9a3e1f0f8d
Labeeb Latheef <43567561+labeebklatif@users.noreply.github.com>
2021-10-29 12:21:44 +0530

    added custom variable node, variable choose menu, variable text styles (#19)
    * custom placeholder extension and custom node for variable support
    
    * Variable choose popup ui, variable text color
    
    * removed commented code
    
    * removed unused variables

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -3,7 +3,6 @@ import classNames from "classnames";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Typography from "@tiptap/extension-typography";
-import Placeholder from "@tiptap/extension-placeholder";
 import Highlight from "@tiptap/extension-highlight";
 import Dropcursor from "@tiptap/extension-dropcursor";
 import Link from "@tiptap/extension-link";
@@ -13,10 +12,12 @@ import TextStyle from "@tiptap/extension-text-style";
 import ImageExtension from "./CustomExtensions/Image/ExtensionConfig";
 import SlashCommands from "./CustomExtensions/SlashCommands/ExtensionConfig";
 import CodeBlock from "./CustomExtensions/CodeBlock/ExtensionConfig";
-import ImageUploader from "./CustomExtensions/Image/Uploader";
+import Variable from "./CustomExtensions/Variable/ExtensionConfig";
+import Placeholder from "./CustomExtensions/Placeholder/ExtensionConfig";
 import BubbleMenu from "./CustomExtensions/BubbleMenu";
 import Embeds from "./CustomExtensions/Embeds";
 import FixedMenu from "./CustomExtensions/FixedMenu";
+import ImageUploader from "./CustomExtensions/Image/Uploader";
 
 import "./styles/EditorStyles.scss";
 
@@ -37,6 +38,7 @@ const Tiptap = (
     initialValue = "",
     onChange = () => {},
     menuType = "fixed",
+    variables,
     ...otherProps
   },
   ref
@@ -46,17 +48,18 @@ const Tiptap = (
     extensions = otherProps.extensions;
   } else {
     extensions = [
+      Variable,
       StarterKit,
       Typography,
       TextStyle,
       Highlight,
-      Placeholder,
       CodeBlock,
       ImageExtension,
       Dropcursor,
       Embeds,
       Link,
       Color,
+      Placeholder,
     ];
   }
 
@@ -90,7 +93,9 @@ const Tiptap = (
 
   return (
     <>
-      {menuType === "fixed" ? <FixedMenu editor={editor} /> : null}
+      {menuType === "fixed" ? (
+        <FixedMenu editor={editor} variables={variables} />
+      ) : null}
       {menuType === "bubble" ? (
         <BubbleMenu editor={editor} formatterOptions={formatterOptions} />
       ) : null}
