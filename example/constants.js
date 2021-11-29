b7b1908da3c3258b224f1693910993903812effd
Labeeb Latheef <43567561+labeebklatif@users.noreply.github.com>
2021-11-30 00:14:01 +0530

    Added props table to documentation (#52)

--- example/constants.js
+++ example/constants.js
@@ -39,6 +39,81 @@ export const SAMPLE_MENTIONS = [
   "Jaden Smith",
 ];
 
+export const PROP_TABLE_COLUMNS = ["Prop", "Description", "Sample Value"];
+
+export const PROP_TABLE_ROWS = [
+  [
+    "ref",
+    "Accepts a React reference. This reference can be used to access TipTap's inbuilt editor methods, such as getHTML()",
+    `React.createRef()`,
+  ],
+  [
+    "initialValue",
+    "Accepts a valid HTML string. This string will be parsed to HTML and will be displayed as initial editor content",
+    `"<p>Hello World</p"`,
+  ],
+  [
+    "onChange",
+    "Accepts a function. This function will be invoked whenever editor content changes, with new content as argument",
+    `(newContent) => {}`,
+  ],
+  [
+    "menuType",
+    "Describes the menu type that editor should display. value should be one of ['fixed', 'bubble']. defaults to 'fixed'",
+    `"bubble"`,
+  ],
+  [
+    "hideSlashCommands",
+    "Accepts a boolean value. When true, the Slash Commands menu will be hidden",
+    `true`,
+  ],
+  [
+    "placeholder",
+    "Accepts a string, an object or a function that returns the placeholder for HTML nodes.",
+    `"Input text here"`,
+  ],
+  [
+    "forceTitle",
+    "Accepts a boolean value. When true, the content is forced to have a title by default.",
+    `true`,
+  ],
+  [
+    "uploadEndpoint",
+    "Accepts an URL endpoint string. This URL will be used for XHR image uploads",
+    `"/api/v1/direct_uploads"`,
+  ],
+  [
+    "variables",
+    "Accepts an array of variable suggestions.",
+    `[{ label: "Subdomain", key: "subdomain" }]`,
+  ],
+  [
+    "mentions",
+    "Accepts an array of mention suggestions.",
+    `[{ label: "Oliver Smith", key: "oliver-smith" }]`,
+  ],
+  [
+    "showImageInMention",
+    "Accepts a boolean value. This value controls the visibility of images in mention suggestions",
+    `true`,
+  ],
+  [
+    "formatterOptions",
+    "Accepts an array of string values. The values are used to filter actions that should be shown in the Bubble menu.",
+    `["bold", "italic", "code", "highlight", "strike", "link"]`,
+  ],
+  [
+    "extensions",
+    "Accepts an array of TipTap extensions. When provided, this will override the default extensions.",
+    `[Bold, Color]`,
+  ],
+  [
+    "className",
+    "Accepts a string value. Can be used for further customisation of the editor content layout.",
+    `"neeto-editor-content"`,
+  ],
+];
+
 export const STRINGS = {
   fixedMenuSampleCode: `
   <Editor />`,
