d3f6ed64314c7aebabc2be09953775412424fc8b
Abhay V Ashokan <35297280+AbhayVAshokan@users.noreply.github.com>
2022-06-06 12:02:12 +0530

    Reduction in bundle size (#250)
    * Added babel plugin for ramda
    
    * Deleted craco config
    
    * Updated postcss and postcss-import plugin

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => {
   api.cache(true);
   return {
     presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: ["@babel/plugin-proposal-class-properties"],
+    plugins: ["@babel/plugin-proposal-class-properties", "ramda"],
     env: {
       production: {
         plugins: [
