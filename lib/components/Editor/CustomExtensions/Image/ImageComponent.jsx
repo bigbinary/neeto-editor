a2753c56543e1e2eeace6c37353619c1beebd513
AbhayVAshokan <abhayvashokan@gmail.com>
2022-07-25 13:23:43 +0530

    Added renderHTML method to update editor content

--- lib/components/Editor/CustomExtensions/Image/ImageComponent.jsx
+++ lib/components/Editor/CustomExtensions/Image/ImageComponent.jsx
@@ -3,16 +3,27 @@ import React from "react";
 import { NodeViewWrapper } from "@tiptap/react";
 import { Resizable } from "re-resizable";
 
-const ImageComponent = ({ node }) => {
-  const { alt, src, float, align } = node.attrs;
+const ImageComponent = ({ node, editor, getPos }) => {
+  const { alt, src, float, align, height, width } = node.attrs;
   const caption = alt || "";
+  const { view } = editor;
 
   return (
     <NodeViewWrapper>
       <Resizable
         lockAspectRatio
         className={`neeto-editor__image neeto-editor__image--${float} neeto-editor__image--${align}`}
-        defaultSize={{ height: "auto", width: 384 }}
+        size={{ height, width }}
+        onResizeStop={(e, direction, ref, d) => {
+          view.dispatch(
+            view.state.tr.setNodeMarkup(getPos(), undefined, {
+              ...node.attrs,
+              height: height + d.height,
+              width: width + d.width,
+            })
+          );
+          editor.commands.focus();
+        }}
       >
         <figure>
           <img alt={caption} src={src} />
