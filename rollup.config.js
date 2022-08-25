d99c21d3a66b1e8ebf842901b6682f2f5e859994
AbhayVAshokan <abhayvashokan@gmail.com>
2022-08-25 11:48:38 +0530

    Added back the generation of editor-content.min.css file

--- rollup.config.js
+++ rollup.config.js
@@ -11,49 +11,66 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import styles from "rollup-plugin-styles";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "./lib/index.js",
-  output: {
-    file: "build/index.js",
-    format: "esm",
-    sourcemap: false,
-    name: "neetoEditor",
-    assetFileNames: "[name][extname]",
+export default [
+  {
+    input: "./lib/index.js",
+    output: {
+      file: "build/index.js",
+      format: "esm",
+      sourcemap: false,
+      name: "neetoEditor",
+      assetFileNames: "[name][extname]",
+    },
+    plugins: [
+      peerDepsExternal(),
+      alias({
+        entries: {
+          apis: path.resolve(__dirname, "lib/apis"),
+          components: path.resolve(__dirname, "lib/components"),
+          hooks: path.resolve(__dirname, "lib/hooks"),
+          constants: path.resolve(__dirname, "lib/constants"),
+          utils: path.resolve(__dirname, "lib/utils"),
+          neetoicons: "@bigbinary/neeto-icons",
+          lib: path.resolve(__dirname, "lib"),
+        },
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: true,
+        extensions: [".js", ".jsx", ".svg"],
+      }),
+      commonjs(),
+      babel({
+        exclude: /node_modules/,
+        extensions: [".jsx", ".js"],
+        presets: ["@babel/preset-env", "@babel/preset-react"],
+        plugins: ["@babel/plugin-transform-runtime"],
+        babelHelpers: "runtime",
+      }),
+      styles({
+        extensions: [".css", ".scss", ".min.css"],
+      }),
+      terser({ compress: { evaluate: false } }),
+      // visualizer(),
+    ],
   },
-  plugins: [
-    peerDepsExternal(),
-    alias({
-      entries: {
-        apis: path.resolve(__dirname, "lib/apis"),
-        components: path.resolve(__dirname, "lib/components"),
-        hooks: path.resolve(__dirname, "lib/hooks"),
-        constants: path.resolve(__dirname, "lib/constants"),
-        utils: path.resolve(__dirname, "lib/utils"),
-        neetoicons: "@bigbinary/neeto-icons",
-        lib: path.resolve(__dirname, "lib"),
-      },
-    }),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-      preventAssignment: true,
-    }),
-    resolve({
-      browser: true,
-      preferBuiltins: true,
-      extensions: [".js", ".jsx", ".svg"],
-    }),
-    commonjs(),
-    babel({
-      exclude: /node_modules/,
-      extensions: [".jsx", ".js"],
-      presets: ["@babel/preset-env", "@babel/preset-react"],
-      plugins: ["@babel/plugin-transform-runtime"],
-      babelHelpers: "runtime",
-    }),
-    styles({
-      extensions: [".css", ".scss", ".min.css"],
-    }),
-    terser({ compress: { evaluate: false } }),
-    // visualizer(),
-  ],
-};
+  {
+    input: "./lib/styles/editor-output.scss",
+    output: {
+      dir: `${__dirname}/dist`,
+      format: "esm",
+      assetFileNames: "[name][extname]",
+    },
+    plugins: [
+      styles({
+        extensions: [".css", ".scss", ".min.css"],
+        mode: ["extract", "editor-content.min.css"],
+        minimize: true,
+      }),
+    ],
+  },
+];
