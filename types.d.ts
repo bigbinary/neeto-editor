2611ce93af21b8de849a4000030e9e20a06645fa
AbhayVAshokan <abhayvashokan@gmail.com>
2022-09-01 16:32:07 +0530

    Added types support to exprted components

---
+++ types.d.ts
@@ -0,0 +1,78 @@
+import { UppyOptions } from "@uppy/core/types";
+import {
+  Editor as TiptapEditor,
+  Extension,
+  Node,
+} from "@tiptap/core/dist/packages/core/src";
+import { Range } from "@tiptap/core/dist/packages/core/src/types";
+import { Transaction } from "prosemirror-state";
+
+interface Command {
+  title: string;
+  Icon: Function;
+  description?: string;
+  optionName: string;
+  command?: (props: { editor: TiptapEditor; range: Range }) => void;
+  items?: Command[];
+}
+
+interface Variable {
+  category_key: string;
+  category_label: string;
+  variables?: Array<{ key: string; label: string }>;
+}
+
+interface Mention {
+  key?: string;
+  name: string;
+  imageUrl?: string;
+}
+
+type KeyboardShortcuts = {
+  [shortcut: string]: (props: { editor: TiptapEditor }) => boolean | void;
+};
+
+type EditorFocus = (props: {
+  editor: TiptapEditor;
+  event: FocusEvent;
+  transaction: Transaction;
+}) => void;
+
+export function Editor(props: {
+  forceTitle?: boolean;
+  titleError?: boolean;
+  hideSlashCommands?: boolean;
+  defaults?: string[];
+  addons?: string[];
+  addonCommands?: Command[];
+  className?: string;
+  uploadEndpoint?: string;
+  uploadConfig?: UppyOptions<Record<string, unknown>>;
+  initialValue?: string;
+  onChange?: (htmlContent: string) => void;
+  onFocus?: EditorFocus;
+  onBlur?: EditorFocus;
+  menuType?: string;
+  variables?: Variable[];
+  mentions?: Mention[];
+  showImageInMention?: boolean;
+  placeholder?: { title: string } | string | null;
+  extensions?: Array<Node | Extension>;
+  contentClassName?: string[];
+  characterLimit?: number;
+  editorSecrets?: Array<{ unsplash?: string }>;
+  rows?: number;
+  autoFocus?: boolean;
+  onSubmit?: (htmlContent: string) => void;
+  heightStrategy?: string;
+  characterCountStrategy?: string;
+  keyboardShortcuts?: KeyboardShortcuts;
+  error?: string;
+  [otherProps: string]: any;
+}): JSX.Element;
+
+export function EditorContent(props: {
+  content?: string;
+  className?: string;
+  [otherProps: string]: any;
+}): JSX.Element;
