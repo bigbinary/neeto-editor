3cd0ac627d196f0087a5374cf56b81b5f08512b9
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-09-15 11:22:06 +0530

    Moved fixed menu options to a constant

--- lib/components/Editor/CustomExtensions/FixedMenu/index.jsx
+++ lib/components/Editor/CustomExtensions/FixedMenu/index.jsx
@@ -4,24 +4,13 @@ import React, { useState } from "react";
 
 import MenuButton from "components/Common/MenuButton";
 import Modal from "components/Common/Modal";
-import {
-  TextBold,
-  TextItalic,
-  Underline,
-  TextCross,
-  Highlight,
-  Code,
-  ListDot,
-  ListNumber,
-  Image,
-  Quote,
-} from "neetoicons";
 import { capitalize } from "utils/common";
 
 import EmojiOption from "./EmojiOption";
 import FontSizeOption from "./FontSizeOption";
 import LinkOption from "./LinkOption";
 import TextColorOption from "./TextColorOption";
+import { buildMenuOptions } from "./utils";
 
 import { getImageMenuOptions } from "../BubbleMenu/helpers";
 import ImageEditor from "../Image/ImageEditor";
@@ -45,79 +34,11 @@ const FixedMenu = ({
     return null;
   }
 
-  const fontStyleOptions = [
-    {
-      Icon: TextBold,
-      command: () => editor.chain().focus().toggleBold().run(),
-      active: editor.isActive("bold"),
-      optionName: "bold",
-    },
-    {
-      Icon: TextItalic,
-      command: () => editor.chain().focus().toggleItalic().run(),
-      active: editor.isActive("italic"),
-      optionName: "italic",
-    },
-    {
-      Icon: Underline,
-      command: () => editor.chain().focus().toggleUnderline().run(),
-      active: editor.isActive("underline"),
-      optionName: "underline",
-    },
-    {
-      Icon: TextCross,
-      command: () => editor.chain().focus().toggleStrike().run(),
-      active: editor.isActive("strike"),
-      optionName: "strike",
-    },
-    {
-      Icon: Highlight,
-      command: () => editor.chain().focus().toggleHighlight().run(),
-      active: editor.isActive("highlight"),
-      optionName: "highlight",
-    },
-  ].filter(item => options.includes(item.optionName));
-
-  const blockStyleOptions = [
-    {
-      Icon: Quote,
-      command: () => editor.chain().focus().toggleBlockquote().run(),
-      active: editor.isActive("blockquote"),
-      optionName: "block-quote",
-      highlight: true,
-    },
-    {
-      Icon: Code,
-      command: () => editor.chain().focus().toggleCodeBlock().run(),
-      active: editor.isActive("codeBlock"),
-      optionName: "code-block",
-      highlight: true,
-    },
-    {
-      Icon: Image,
-      command: () => setImageUploadVisible(true),
-      active: editor.isActive("imageUpload"),
-      optionName: "image-upload",
-    },
-  ].filter(item => options.includes(item.optionName));
-
-  const listStyleOptions = [
-    {
-      Icon: ListDot,
-      command: () => editor.chain().focus().toggleBulletList().run(),
-      active: editor.isActive("bulletList"),
-      optionName: "bullet-list",
-      highlight: true,
-    },
-    {
-      Icon: ListNumber,
-      command: () => editor.chain().focus().toggleOrderedList().run(),
-      active: editor.isActive("orderedList"),
-      optionName: "ordered-list",
-      highlight: true,
-    },
-  ].filter(item => options.includes(item.optionName));
-
+  const {
+    font: fontStyleOptions,
+    block: blockStyleOptions,
+    list: listStyleOptions,
+  } = buildMenuOptions({ editor, options, setImageUploadVisible });
   const fontSizeOptions = options.filter(option => option.match(/^h[1-6]$/));
   const isTextColorActive = options.includes(EDITOR_OPTIONS.FONT_COLOR);
   const isFontSizeActive = fontSizeOptions.length > 0;
