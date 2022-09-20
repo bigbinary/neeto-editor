eeeb55297ba7b4377e58b6d52b08df9f1e78f84f
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-09-20 11:17:31 +0530

    Added h3 option to the list of available headings

--- lib/components/Editor/CustomExtensions/FixedMenu/FontSizeOption.jsx
+++ lib/components/Editor/CustomExtensions/FixedMenu/FontSizeOption.jsx
@@ -1,4 +1,4 @@
-import { EDITOR_OPTIONS } from "constants/common";
+import { FONT_SIZE_OPTIONS } from "constants/common";
 
 import React from "react";
 
@@ -7,16 +7,7 @@ import Dropdown from "components/Common/Dropdown";
 import MenuButton from "components/Common/MenuButton";
 import { TextSize } from "neetoicons";
 
-const FontSizeOption = ({ editor, options }) => {
-  const fontSizeOptions = {
-    [EDITOR_OPTIONS.H1]: { label: "H1", value: 1 },
-    [EDITOR_OPTIONS.H2]: { label: "H2", value: 2 },
-    [EDITOR_OPTIONS.H3]: { label: "H3", value: 3 },
-    [EDITOR_OPTIONS.H4]: { label: "H4", value: 4 },
-    [EDITOR_OPTIONS.H5]: { label: "H5", value: 5 },
-    [EDITOR_OPTIONS.H6]: { label: "H6", value: 6 },
-  };
-
+const FontSizeOption = ({ editor }) => {
   const isActive = level => editor.isActive("heading", { level });
 
   const onClick = level =>
@@ -39,19 +30,16 @@ const FontSizeOption = ({ editor, options }) => {
       className="neeto-editor-font-size-wrapper"
       placement="bottom"
     >
-      {options.map(option => {
-        const { label, value } = fontSizeOptions[option];
-        return (
-          <li
-            onClick={() => onClick(value)}
-            key={value}
-            className={classnames({ active: isActive(value) })}
-            data-cy={`neeto-editor-fixed-menu-font-size-option-${label}`}
-          >
-            {label}
-          </li>
-        );
-      })}
+      {FONT_SIZE_OPTIONS.map(({ label, value }) => (
+        <li
+          onClick={() => onClick(value)}
+          key={value}
+          className={classnames({ active: isActive(value) })}
+          data-cy={`neeto-editor-fixed-menu-font-size-option-${label}`}
+        >
+          {label}
+        </li>
+      ))}
     </Dropdown>
   );
 };
