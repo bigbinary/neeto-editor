eefd1e014718398da75759c3f23f09d19a6a741b
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-09-21 12:41:30 +0530

    Added font type dropdown to bubble menu

--- lib/components/Editor/CustomExtensions/BubbleMenu/TextOptions.jsx
+++ lib/components/Editor/CustomExtensions/BubbleMenu/TextOptions.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { getTextMenuOptions } from "./helpers";
+import Tippy from "@tippyjs/react";
+import { Down } from "neetoicons";
+
+import {
+  getNodeType,
+  getTextMenuDefaultOptions,
+  getTextMenuDropdownOptions,
+} from "./helpers";
 import LinkOption from "./LinkOption";
 import Option from "./Option";
 
@@ -11,6 +18,15 @@ const TextOptions = ({
   isLinkOptionActive,
   setIsLinkOptionActive,
 }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const fixedOptions = getTextMenuDefaultOptions({
+    editor,
+    setIsLinkOptionActive,
+  }).filter(({ optionName }) => options.includes(optionName));
+  const dropdownOptions = getTextMenuDropdownOptions({ editor });
+  const nodeType = getNodeType(dropdownOptions);
+
   const handleAnimateInvalidLink = () => {
     setIsInvalidLink(true);
     setTimeout(() => {
@@ -18,19 +34,50 @@ const TextOptions = ({
     }, 1000);
   };
 
-  return isLinkOptionActive ? (
-    <LinkOption
-      editor={editor}
-      handleClose={() => setIsLinkOptionActive(false)}
-      handleAnimateInvalidLink={handleAnimateInvalidLink}
-    />
-  ) : (
-    getTextMenuOptions({
-      editor,
-      setIsLinkOptionActive,
-    })
-      .filter(({ optionName }) => options.includes(optionName))
-      .map(option => <Option {...option} key={option.optionName} />)
+  const handleClose = () => setIsDropdownOpen(false);
+
+  if (isLinkOptionActive) {
+    return (
+      <LinkOption
+        editor={editor}
+        handleClose={() => setIsLinkOptionActive(false)}
+        handleAnimateInvalidLink={handleAnimateInvalidLink}
+      />
+    );
+  }
+
+  return (
+    <>
+      <Tippy
+        interactive
+        placement="bottom"
+        visible={isDropdownOpen}
+        onClickOutside={handleClose}
+        content={dropdownOptions.map(({ optionName, command }) => (
+          <button
+            key={optionName}
+            onClick={() => {
+              command();
+              handleClose();
+            }}
+            className="neeto-editor-bubble-menu__item neeto-editor-bubble-menu__dropdown"
+          >
+            {optionName}
+          </button>
+        ))}
+      >
+        <button
+          onClick={() => setIsDropdownOpen(open => !open)}
+          className="neeto-editor-bubble-menu__item neeto-editor-bubble-menu__dropdown-target"
+        >
+          {nodeType}
+          <Down size={14} />
+        </button>
+      </Tippy>
+      {fixedOptions.map(option => (
+        <Option {...option} key={option.optionName} />
+      ))}
+    </>
   );
 };
 
