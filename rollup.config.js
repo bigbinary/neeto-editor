a819ef022941327bdf027e94d6f42806bc5d8a74
Abhay V Ashokan <35297280+AbhayVAshokan@users.noreply.github.com>
2022-09-28 12:53:49 +0530

    Exposed code block highlight feature to CDN (#373)

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,42 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import styles from "rollup-plugin-styles";
 import { terser } from "rollup-plugin-terser";
 
+const plugins = [
+  peerDepsExternal(),
+  alias({
+    entries: {
+      apis: path.resolve(__dirname, "lib/apis"),
+      components: path.resolve(__dirname, "lib/components"),
+      hooks: path.resolve(__dirname, "lib/hooks"),
+      constants: path.resolve(__dirname, "lib/constants"),
+      utils: path.resolve(__dirname, "lib/utils"),
+      neetoicons: "@bigbinary/neeto-icons",
+      lib: path.resolve(__dirname, "lib"),
+    },
+  }),
+  replace({
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    preventAssignment: true,
+  }),
+  resolve({
+    browser: true,
+    preferBuiltins: true,
+    extensions: [".js", ".jsx", ".svg"],
+  }),
+  commonjs({ include: /\**node_modules\**/ }),
+  babel({
+    exclude: /node_modules/,
+    extensions: [".jsx", ".js"],
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: ["@babel/plugin-transform-runtime"],
+    babelHelpers: "runtime",
+  }),
+  styles({
+    extensions: [".css", ".scss", ".min.css"],
+  }),
+  terser({ compress: { evaluate: false } }),
+];
+
 export default [
   {
     input: "./lib/index.js",
@@ -19,42 +55,7 @@ export default [
       name: "neetoEditor",
       assetFileNames: "[name][extname]",
     },
-    plugins: [
-      peerDepsExternal(),
-      alias({
-        entries: {
-          apis: path.resolve(__dirname, "lib/apis"),
-          components: path.resolve(__dirname, "lib/components"),
-          hooks: path.resolve(__dirname, "lib/hooks"),
-          constants: path.resolve(__dirname, "lib/constants"),
-          utils: path.resolve(__dirname, "lib/utils"),
-          neetoicons: "@bigbinary/neeto-icons",
-          lib: path.resolve(__dirname, "lib"),
-        },
-      }),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("production"),
-        preventAssignment: true,
-      }),
-      resolve({
-        browser: true,
-        preferBuiltins: true,
-        extensions: [".js", ".jsx", ".svg"],
-      }),
-      commonjs({ include: /\**node_modules\**/ }),
-      babel({
-        exclude: /node_modules/,
-        extensions: [".jsx", ".js"],
-        presets: ["@babel/preset-env", "@babel/preset-react"],
-        plugins: ["@babel/plugin-transform-runtime"],
-        babelHelpers: "runtime",
-      }),
-      styles({
-        extensions: [".css", ".scss", ".min.css"],
-      }),
-      terser({ compress: { evaluate: false } }),
-      // visualizer(),
-    ],
+    plugins,
   },
   {
     input: "./lib/styles/editor-output.scss",
@@ -70,5 +71,15 @@ export default [
         minimize: true,
       }),
     ],
+  },
+  {
+    input: "./lib/utils/codeBlockHighlight.js",
+    output: {
+      dir: `${__dirname}/dist`,
+      format: "cjs",
+      sourcemap: false,
+      assetFileNames: "[name][extname]",
+    },
+    plugins,
   },
 ];
