3ad885f81c5f0d122bb97421700eca4ad776d66d
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-10-05 12:17:27 +0530

    Whitelisted iframe in DOMPurify

--- lib/components/Editor/EditorContent.jsx
+++ lib/components/Editor/EditorContent.jsx
@@ -1,9 +1,9 @@
-import { EDITOR_CONTENT_CLASSNAME } from "constants/common";
-
 import React from "react";
 
 import classnames from "classnames";
 import DOMPurify from "dompurify";
+
+import { EDITOR_CONTENT_CLASSNAME } from "constants/common";
 import highlightCode from "utils/highlightCode";
 
 const EditorContent = ({ content = "", className, ...otherProps }) => {
@@ -16,7 +16,10 @@ const EditorContent = ({ content = "", className, ...otherProps }) => {
         [className]: className,
       })}
       dangerouslySetInnerHTML={{
-        __html: sanitize(htmlContent, { ADD_ATTR: ["target"] }),
+        __html: sanitize(htmlContent, {
+          ADD_TAGS: ["iframe"],
+          ADD_ATTR: ["target", "allow", "allowfullscreen", "frameborder"],
+        }),
       }}
       data-cy="neeto-editor-content"
       {...otherProps}
