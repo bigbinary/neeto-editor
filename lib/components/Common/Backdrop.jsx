47bd741280a081f053471e011ec1a205ca97ae21
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-12-18 16:03:42 +0530

    Replaced the Modal component

--- lib/components/Common/Backdrop.jsx
+++
@@ -1,9 +0,0 @@
-import React, { forwardRef } from "react";
-
-const Portal = ({ children, ...otherProps }, ref) => (
-  <div data-testid="ne-backdrop" ref={ref} {...otherProps}>
-    {children}
-  </div>
-);
-
-export default forwardRef(Portal);
