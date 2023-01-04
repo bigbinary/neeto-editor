78815dec0a74c18deafd2813fc2f8e88d6ff0b76
Gaagul Gigi <gaagulgigi@gmail.com>
2023-01-04 12:31:44 +0530

    Added neetocommon functions to utils

--- lib/components/Attachments/AttachmentProgress.jsx
+++ lib/components/Attachments/AttachmentProgress.jsx
@@ -9,18 +9,13 @@ const AttachmentProgress = ({ attachment }) => {
   const progressPercentage = `${attachment.progress}%`;
 
   return (
-    <div className="ne-file-attachment-card-wrapper">
-      <div className="ne-file-attachment-inner-wrapper">
-        <File className="icon-opacity-50" size={25} />
-        <div className="ne-file-attachment-progress-wrapper">
-          <Typography className="truncate-ellipsis-progress">
-            {attachment.filename}
-          </Typography>
-          <Typography>{progressPercentage}</Typography>
-          <Button icon={Close} style="text" onClick={noop} />
-        </div>
+    <div className="ne-attachments__attachment">
+      <File className="ne-attachments__attachment__progress-icon" size={16} />
+      <div className="ne-attachments__attachment__progress">
+        <Typography style="body2">{attachment.filename}</Typography>
+        <Typography style="body2">{progressPercentage}</Typography>
       </div>
-      <Button icon={Close} size="small" style="text" onClick={() => {}} />
+      <Button icon={Close} size="small" style="text" onClick={noop} />
     </div>
   );
 };
