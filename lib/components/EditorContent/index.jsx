5ea86cbdc71d969872d663a44ffdc35d54244cc0
Gaagul Gigi <gaagulgigi@gmail.com>
2023-01-19 08:47:50 +0530

    Optimized using neeto commons

--- lib/components/EditorContent/index.jsx
+++ lib/components/EditorContent/index.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import classnames from "classnames";
 import DOMPurify from "dompurify";
 
-import { isNilOrEmpty } from "utils/common";
-
 import { EDITOR_CONTENT_CLASSNAME, SANITIZE_OPTIONS } from "./constants";
 import { highlightCode, substituteVariables } from "./utils";
 
@@ -14,10 +12,7 @@ const EditorContent = ({
   className,
   ...otherProps
 }) => {
-  const highlightedContent = highlightCode(content);
-  const htmlContent = !isNilOrEmpty(variables)
-    ? substituteVariables(highlightedContent, variables)
-    : highlightedContent;
+  const htmlContent = substituteVariables(highlightCode(content), variables);
   const sanitize = DOMPurify.sanitize;
 
   return (
