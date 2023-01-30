10efb96b07852780be3988206057c81b4f55b9bd
Gaagul Gigi <gaagulgigi@gmail.com>
2023-01-30 16:12:01 +0530

    Improved validation

--- lib/components/Editor/EmbedOption/index.jsx
+++ lib/components/Editor/EmbedOption/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { Button, Input, Modal, Typography } from "neetoui";
+import { isEmpty } from "ramda";
 
 import { validateUrl } from "./utils";
 
@@ -8,25 +9,50 @@ const EmbedOption = ({ isEmbedModalOpen, setIsEmbedModalOpen, editor }) => {
   const [embedUrl, setEmbedUrl] = useState("");
   const [error, setError] = useState(false);
 
+  const inputRef = useRef(null);
+
   const handleEmbed = () => {
-    editor.chain().focus().setExternalVideo({ src: embedUrl }).run();
-    setEmbedUrl("");
-    setIsEmbedModalOpen(false);
+    const validatedUrl = validateUrl(embedUrl);
+
+    if (validatedUrl) {
+      editor.chain().focus().setExternalVideo({ src: validatedUrl }).run();
+      setEmbedUrl("");
+      setIsEmbedModalOpen(false);
+    } else {
+      setError(true);
+    }
   };
 
   const handleChange = e => {
-    const validatedUrl = validateUrl(e.target.value);
-    if (validatedUrl) {
+    if (validateUrl(e.target.value)) {
       setError(false);
-      setEmbedUrl(validatedUrl);
+      setEmbedUrl(e.target.value);
     } else {
       setError(true);
       setEmbedUrl(e.target.value);
     }
   };
 
+  const handleClose = () => {
+    setEmbedUrl("");
+    setError(false);
+    setIsEmbedModalOpen(false);
+  };
+
+  const handleKeyDown = e => {
+    if (e.key === "Enter") {
+      handleEmbed();
+    } else if (e.key === "Escape") {
+      handleClose();
+    }
+  };
+
   return (
-    <Modal isOpen={isEmbedModalOpen} onClose={() => setIsEmbedModalOpen(false)}>
+    <Modal
+      initialFocusRef={inputRef}
+      isOpen={isEmbedModalOpen}
+      onClose={handleClose}
+    >
       <Modal.Header>
         <Typography id="dialog1Title" style="h2">
           Embed Video
@@ -34,16 +60,22 @@ const EmbedOption = ({ isEmbedModalOpen, setIsEmbedModalOpen, editor }) => {
       </Modal.Header>
       <Modal.Body className="space-y-2">
         <Input
-          error={error ? "Please enter a valid URL format" : null}
+          error={error ? "Please enter a valid URL" : null}
           label="Video URL:"
+          ref={inputRef}
           size="medium"
           type="text"
           value={embedUrl}
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
         />
       </Modal.Body>
       <Modal.Footer className="space-x-2">
-        <Button disabled={error} label="Embed" onClick={handleEmbed} />
+        <Button
+          disabled={error || isEmpty(embedUrl)}
+          label="Embed"
+          onClick={handleEmbed}
+        />
       </Modal.Footer>
     </Modal>
   );
