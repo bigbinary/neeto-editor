4482d3065144a90d43436c9dd5b51ca0ed625b41
Gaagul Gigi <gaagulgigi@gmail.com>
2023-02-13 13:37:40 +0530

    Added special mention feature

---
+++ lib/components/Editor/CustomExtensions/SpecialMentions/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { Email } from "neetoicons";
+import { Avatar, Dropdown, Typography } from "neetoui";
+import { isEmpty } from "ramda";
+
+import { formatMentions } from "./utils";
+
+const SpecialMentions = ({ editor, mentions, tooltipContent }) => {
+  const formattedMentions = formatMentions(mentions);
+  const { Menu, MenuItem } = Dropdown;
+
+  if (isEmpty(formattedMentions)) return null;
+
+  return (
+    <Dropdown
+      buttonStyle="text"
+      data-cy="neeto-editor-mention-option"
+      icon={Email}
+      strategy="fixed"
+      buttonProps={{
+        tooltipProps: {
+          content: tooltipContent,
+          position: "bottom",
+        },
+        className: "neeto-editor-fixed-menu__item",
+      }}
+    >
+      <Menu>
+        {formattedMentions.map(({ key, name, imageUrl }) => (
+          <MenuItem.Button
+            data-cy={`neeto-editor-mention-option-${key}`}
+            key={key}
+            onClick={() => editor.commands.setMention({ id: key, label: name })}
+          >
+            <Avatar size="small" user={{ name, imageUrl }} />
+            <Typography style="body2">{name}</Typography>
+          </MenuItem.Button>
+        ))}
+      </Menu>
+    </Dropdown>
+  );
+};
+
+export default SpecialMentions;
