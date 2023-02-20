6e8763657d8a0f56ed660066ede2ada02eed33ea
Gaagul Gigi <gaagulgigi@gmail.com>
2023-02-20 08:04:11 +0530

    Changed button to Tag in variables menu

--- lib/components/Editor/CustomExtensions/Variable/VariableList.jsx
+++ lib/components/Editor/CustomExtensions/Variable/VariableList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Tag } from "neetoui";
 import { isEmpty } from "ramda";
 
 import { parseVariables } from "./utils";
@@ -34,15 +35,15 @@ const VariableCategory = ({ index, title, variables, onClickItem }) => (
     {title && <h6>{title}</h6>}
     <div className="neeto-editor-variables-row">
       {variables.map(item => (
-        <button
-          className="neeto-editor-variable"
+        <Tag
           data-cy={`neeto-editor-variable-option-item-${item.label}--${item.value}`}
           key={`${item.label}--${item.value}`}
-          type="button"
+          label={item.label}
+          style="secondary"
+          className="neeto-ui-text-black
+                    neeto-ui-border-gray-300 hover:neeto-ui-text-primary-800 hover:neeto-ui-border-primary-800 neeto-ui-leading-snug cursor-pointer rounded-md px-2"
           onClick={() => onClickItem(item)}
-        >
-          {item.label}
-        </button>
+        />
       ))}
     </div>
   </div>
