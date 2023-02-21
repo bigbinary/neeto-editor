55e1a782eed42aed79b00048a43193160c922a20
Gaagul Gigi <gaagulgigi@gmail.com>
2023-02-21 18:45:21 +0530

    Replaced tag with custom button

--- lib/components/Editor/CustomExtensions/Variable/VariableList.jsx
+++ lib/components/Editor/CustomExtensions/Variable/VariableList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Tag } from "neetoui";
 import { isEmpty } from "ramda";
 
 import { parseVariables } from "./utils";
@@ -35,15 +34,14 @@ const VariableCategory = ({ index, title, variables, onClickItem }) => (
     {title && <h6>{title}</h6>}
     <div className="neeto-editor-variables-row">
       {variables.map(item => (
-        <Tag
+        <button
+          className="neeto-editor-variables-item"
           data-cy={`neeto-editor-variable-option-item-${item.label}--${item.value}`}
           key={`${item.label}--${item.value}`}
-          label={item.label}
-          style="secondary"
-          className="neeto-ui-text-black
-                    neeto-ui-border-gray-300 hover:neeto-ui-text-primary-800 hover:neeto-ui-border-primary-800 neeto-ui-leading-snug cursor-pointer px-2 ne_variable-tag"
           onClick={() => onClickItem(item)}
-        />
+        >
+          {item.label}
+        </button>
       ))}
     </div>
   </div>
