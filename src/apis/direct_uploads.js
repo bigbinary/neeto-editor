de42606d312a760684f42166ae7d22a091cc8d36
Gaagul Gigi <gaagulgigi@gmail.com>
2023-03-24 17:03:07 +0530

    Moved new instance initialization outside create function

--- src/apis/direct_uploads.js
+++ src/apis/direct_uploads.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
+const axiosWithoutShowErrorInterceptor = axios.create();
+
+// remove the showErrorToaster interceptor from the interceptors list
+axiosWithoutShowErrorInterceptor.interceptors.request.use(config => {
+  const interceptors = axiosWithoutShowErrorInterceptor.interceptors;
+  const showErrorInterceptor = interceptors.request.handlers.find(
+    interceptor => interceptor.fulfilled.name === "showErrorToaster"
+  );
+  interceptors.request.eject(showErrorInterceptor);
+
+  return config;
+});
+
 const generate = (url, payload, config) =>
   axios.post(url, payload, {
     ...config,
@@ -8,27 +21,13 @@ const generate = (url, payload, config) =>
     showToastr: false,
   });
 
-const create = (url, file, config) => {
-  const axiosWithoutShowErrorInterceptor = axios.create();
-
-  // remove the showErrorToaster interceptor from the interceptors list
-  axiosWithoutShowErrorInterceptor.interceptors.request.use(config => {
-    const interceptors = axiosWithoutShowErrorInterceptor.interceptors;
-    const showErrorInterceptor = interceptors.request.handlers.find(
-      interceptor => interceptor.fulfilled.name === "showErrorToaster"
-    );
-    interceptors.request.eject(showErrorInterceptor);
-
-    return config;
-  });
-
-  return axiosWithoutShowErrorInterceptor.put(url, file, {
+const create = (url, file, config) =>
+  axiosWithoutShowErrorInterceptor.put(url, file, {
     ...config,
     transformRequestCase: false,
     transformResponseCase: false,
     showToastr: false,
   });
-};
 
 const update = ({ url, signedId, payload }) =>
   axios.patch(`${url}/${signedId}/`, payload, {
