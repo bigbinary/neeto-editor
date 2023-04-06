3324297f7b59bfa1c2880b6276a581a3ff3e0528
Gaagul Gigi <gaagulgigi@gmail.com>
2023-04-06 16:03:38 +0530

    Added necessary fallbacks for neeto commons

--- resolve.js
+++ resolve.js
@@ -4,4 +4,12 @@ module.exports = {
   alias: {
     common: path.resolve(__dirname, "./src/common"),
   },
+  fallback: {
+    util: require.resolve("util/"),
+    url: require.resolve("url/"),
+    fs: false,
+    buffer: require.resolve("buffer/"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+  },
 };
