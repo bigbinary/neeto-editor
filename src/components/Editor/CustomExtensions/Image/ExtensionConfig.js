2a2560ea83dc304e67e2c9cf34546aca90982bc9
Gaagul Gigi <gaagulgigi@gmail.com>
2023-06-01 12:56:09 +0530

    Added placeholder while uploading after copy-pasing images into editor content

--- src/components/Editor/CustomExtensions/Image/ExtensionConfig.js
+++ src/components/Editor/CustomExtensions/Image/ExtensionConfig.js
@@ -174,24 +174,23 @@ export default {
 
                   images.forEach(async image => {
                     const id = Math.random().toString(36).substring(7);
-                    const node = schema.nodes.image.create({ id });
+                    const node = schema.nodes.image.create({
+                      id,
+                    });
                     const transaction = view.state.tr.insert(pos, node);
                     view.dispatch(transaction);
-                    try {
-                      const url = await upload(image, uploadEndpoint);
-                      url &&
-                        view.state.doc.descendants((node, pos) => {
-                          if (node.attrs.id === id) {
-                            const transaction = view.state.tr.setNodeMarkup(
-                              pos,
-                              null,
-                              { src: url }
-                            );
-                            view.dispatch(transaction);
-                          }
-                        });
-                    } catch {
-                      view.dispatch(view.state.tr.delete(pos, pos + 1));
+                    const url = await upload(image, uploadEndpoint);
+                    if (url) {
+                      view.state.doc.descendants((node, pos) => {
+                        if (node.attrs.id === id) {
+                          const transaction = view.state.tr.setNodeMarkup(
+                            pos,
+                            null,
+                            { src: url }
+                          );
+                          view.dispatch(transaction);
+                        }
+                      });
                     }
                   });
                 },
