621a8c95ca4a1d6ed2a4d1ee11adbcf5d9e81f7b
josephmathew900 <josephmathew900@gmail.com>
2023-10-30 10:41:43 +0530

    Upgraded neeto-commons-frontend and removed i18n initializer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {
 } from "components/Editor/utils";
 import { substituteVariables } from "components/EditorContent/utils";
 import { isEditorEmpty } from "utils/common";
-import { initializeI18n } from "utils/i18next";
 
 import Attachments from "./components/Attachments";
 import Editor from "./components/Editor";
@@ -12,9 +11,6 @@ import FormikEditor from "./components/Editor/FormikEditor";
 import Menu from "./components/Editor/Menu";
 import EditorContent from "./components/EditorContent";
 import "./index.scss";
-import en from "./translations/en.json";
-
-initializeI18n({ translationResources: { en: { translation: en } } });
 
 export {
   Editor,
