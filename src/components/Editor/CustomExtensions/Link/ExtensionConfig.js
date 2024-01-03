e17918a92514b33a1dd222f4858465344151e211
Gaagul Gigi <gaagulgigi@gmail.com>
2024-01-03 14:04:21 +0530

    Added markdown for links

--- src/components/Editor/CustomExtensions/Link/ExtensionConfig.js
+++ src/components/Editor/CustomExtensions/Link/ExtensionConfig.js
@@ -1,3 +1,75 @@
-import Link from "@tiptap/extension-link";
+const {
+  InputRule,
+  markInputRule,
+  markPasteRule,
+  PasteRule,
+} = require("@tiptap/core");
+const { Link } = require("@tiptap/extension-link");
 
-export default Link.extend({ inclusive: false });
+const inputRegex = /(?:^|\s)\[([^\]]*)?\]\((\S+)(?: ["“](.+)["”])?\)$/i;
+const pasteRegex = /(?:^|\s)\[([^\]]*)?\]\((\S+)(?: ["“](.+)["”])?\)/gi;
+
+function linkInputRule(config) {
+  const defaultMarkInputRule = markInputRule(config);
+
+  return new InputRule({
+    find: config.find,
+    handler(props) {
+      const { tr } = props.state;
+
+      defaultMarkInputRule.handler(props);
+      tr.setMeta("preventAutolink", true);
+    },
+  });
+}
+
+function linkPasteRule(config) {
+  const defaultMarkPasteRule = markPasteRule(config);
+
+  return new PasteRule({
+    find: config.find,
+    handler(props) {
+      const { tr } = props.state;
+
+      defaultMarkPasteRule.handler(props);
+      tr.setMeta("preventAutolink", true);
+    },
+  });
+}
+
+const RichTextLink = Link.extend({
+  inclusive: false,
+  addAttributes() {
+    return { ...this.parent?.(), title: { default: null } };
+  },
+  addInputRules() {
+    return [
+      linkInputRule({
+        find: inputRegex,
+        type: this.type,
+        getAttributes(match) {
+          return {
+            title: match.pop()?.trim(),
+            href: match.pop()?.trim(),
+          };
+        },
+      }),
+    ];
+  },
+  addPasteRules() {
+    return [
+      linkPasteRule({
+        find: pasteRegex,
+        type: this.type,
+        getAttributes(match) {
+          return {
+            title: match.pop()?.trim(),
+            href: match.pop()?.trim(),
+          };
+        },
+      }),
+    ];
+  },
+});
+
+export default RichTextLink;
