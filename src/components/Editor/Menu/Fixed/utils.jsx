d8a9e454b0608be4964a45d2862baf04301eb091
Deepak Jose <deepakjosp@gmail.com>
2024-05-29 13:03:10 +0530

    Refactored and added necessary props to the menus in the create menu options in font, history, list and block menu groups.

--- src/components/Editor/Menu/Fixed/utils.jsx
+++ src/components/Editor/Menu/Fixed/utils.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { t } from "i18next";
+import { isNotEmpty } from "neetocist";
 import {
   TextBold,
   TextItalic,
@@ -23,6 +24,7 @@ import {
 import { Button } from "neetoui";
 import { fromPairs, not, assoc } from "ramda";
 
+import { EDITOR_OPTIONS } from "src/common/constants";
 import { generateFocusProps } from "utils/focusHighlighter";
 
 export const createMenuOptions = ({
@@ -32,139 +34,166 @@ export const createMenuOptions = ({
   attachmentProps,
   setIsEmbedModalOpen,
   setIsAddLinkActive,
-}) => ({
-  font: [
-    {
-      Icon: TextBold,
-      command: () => editor.chain().focus().toggleBold().run(),
-      active: editor.isActive("bold"),
-      optionName: "bold",
-      tooltip: tooltips.bold || t("neetoEditor.menu.bold"),
-    },
-    {
-      Icon: TextItalic,
-      command: () => editor.chain().focus().toggleItalic().run(),
-      active: editor.isActive("italic"),
-      optionName: "italic",
-      tooltip: tooltips.italic || t("neetoEditor.menu.italic"),
-    },
-    {
-      Icon: Underline,
-      command: () => editor.chain().focus().toggleUnderline().run(),
-      active: editor.isActive("underline"),
-      optionName: "underline",
-      tooltip: tooltips.underline || t("neetoEditor.menu.underline"),
-    },
-    {
-      Icon: Link,
-      command: () => setIsAddLinkActive(not),
-      optionName: "link",
-      tooltip: "Link",
-    },
-    {
-      Icon: TextCross,
-      command: () => editor.chain().focus().toggleStrike().run(),
-      active: editor.isActive("strike"),
-      optionName: "strike",
-      tooltip: tooltips.strike || t("neetoEditor.menu.strike"),
-    },
-    {
-      Icon: Highlight,
-      command: () => editor.chain().focus().toggleHighlight().run(),
-      active: editor.isActive("highlight"),
-      optionName: "highlight",
-      tooltip: tooltips.highlight || t("neetoEditor.menu.highlight"),
-    },
-  ],
-  block: [
-    {
-      Icon: Quote,
-      command: () => editor.chain().focus().toggleBlockquote().run(),
-      active: editor.isActive("blockquote"),
-      optionName: "block-quote",
-      highlight: true,
-      tooltip: tooltips.blockQuote || t("neetoEditor.menu.blockQuote"),
-    },
-    {
-      Icon: Code,
-      command: () => editor.chain().focus().toggleCode().run(),
-      active: editor.isActive("code"),
-      optionName: "code",
-      tooltip: tooltips.code || t("neetoEditor.menu.code"),
-    },
-    {
-      Icon: CodeBlock,
-      command: () => editor.chain().focus().toggleCodeBlock().run(),
-      active: editor.isActive("codeBlock"),
-      optionName: "code-block",
-      tooltip: tooltips.codeBlock || t("neetoEditor.menu.codeBlock"),
-    },
-  ],
-  list: [
-    {
-      Icon: ListDot,
-      command: () => editor.chain().focus().toggleBulletList().run(),
-      active: editor.isActive("bulletList"),
-      optionName: "bullet-list",
-      highlight: true,
-      tooltip: tooltips.bulletList || t("neetoEditor.menu.bulletedList"),
-    },
-    {
-      Icon: ListNumber,
-      command: () => editor.chain().focus().toggleOrderedList().run(),
-      active: editor.isActive("orderedList"),
-      optionName: "ordered-list",
-      highlight: true,
-      tooltip: tooltips.orderedList || t("neetoEditor.menu.orderedList"),
-    },
-  ],
-  misc: [
-    {
-      Icon: Attachment,
-      command: attachmentProps?.handleUploadAttachments,
-      disabled: attachmentProps?.isDisabled,
-      active: false,
-      optionName: "attachments",
-      tooltip: tooltips.attachments || t("neetoEditor.menu.attachments"),
-    },
-    {
-      Icon: ImageUpload,
-      command: () => setMediaUploader(assoc("image", true)),
-      optionName: "image-upload",
-      tooltip: tooltips.imageUpload || t("neetoEditor.menu.imageUpload"),
-    },
-    {
-      Icon: Video,
-      command: () => setMediaUploader(assoc("video", true)),
-      optionName: "video-upload",
-      tooltip: tooltips.videoUpload || t("neetoEditor.menu.videoUpload"),
-    },
-    {
-      Icon: MediaVideo,
-      command: () => setIsEmbedModalOpen(true),
-      optionName: "video-embed",
-      tooltip: tooltips.videoEmbed || t("neetoEditor.menu.videoEmbed"),
-    },
-  ],
-  right: [
-    {
-      Icon: Undo,
-      command: () => editor.chain().focus().undo().run(),
-      active: false,
-      disabled: !editor.can().undo(),
-      optionName: "undo",
-      tooltip: tooltips.undo || t("neetoEditor.menu.undo"),
-    },
-    {
-      Icon: Redo,
-      command: () => editor.chain().focus().redo().run(),
-      active: false,
-      disabled: !editor.can().redo(),
-      optionName: "redo",
-      tooltip: tooltips.redo || t("neetoEditor.menu.redo"),
-    },
-  ],
-});
+  options,
+}) => {
+  const fontSizeOptions = options.filter(option => option.match(/^h[1-6]$/));
+
+  return {
+    history: [
+      {
+        icon: Undo,
+        command: editor.chain().focus().undo().run,
+        isEnabled: options.includes(EDITOR_OPTIONS.UNDO),
+        active: false,
+        disabled: !editor.can().undo(),
+        optionName: EDITOR_OPTIONS.UNDO,
+        label: tooltips.undo ?? t("neetoEditor.menu.undo"),
+      },
+      {
+        icon: Redo,
+        command: editor.chain().focus().redo().run,
+        isEnabled: options.includes(EDITOR_OPTIONS.REDO),
+        active: false,
+        disabled: !editor.can().redo(),
+        optionName: EDITOR_OPTIONS.REDO,
+        label: tooltips.redo ?? t("neetoEditor.menu.redo"),
+      },
+    ],
+    fontSize: [
+      {
+        type: "fontSize",
+        options: fontSizeOptions,
+        label: tooltips.fontSize ?? t("neetoEditor.menu.fontSize"),
+        isEnabled: isNotEmpty(fontSizeOptions),
+      },
+    ],
+    font: [
+      {
+        icon: TextBold,
+        isEnabled: options.includes(EDITOR_OPTIONS.BOLD),
+        command: editor.chain().focus().toggleBold().run,
+        active: editor.isActive(EDITOR_OPTIONS.BOLD),
+        optionName: EDITOR_OPTIONS.BOLD,
+        label: tooltips.bold ?? t("neetoEditor.menu.bold"),
+      },
+      {
+        icon: TextItalic,
+        isEnabled: options.includes(EDITOR_OPTIONS.ITALIC),
+        command: editor.chain().focus().toggleItalic().run,
+        active: editor.isActive(EDITOR_OPTIONS.ITALIC),
+        optionName: EDITOR_OPTIONS.ITALIC,
+        label: tooltips.italic ?? t("neetoEditor.menu.italic"),
+      },
+      {
+        icon: Underline,
+        isEnabled: options.includes(EDITOR_OPTIONS.UNDERLINE),
+        command: editor.chain().focus().toggleUnderline().run,
+        active: editor.isActive(EDITOR_OPTIONS.UNDERLINE),
+        optionName: EDITOR_OPTIONS.UNDERLINE,
+        label: tooltips.underline ?? t("neetoEditor.menu.underline"),
+      },
+      {
+        icon: Link,
+        isEnabled: options.includes(EDITOR_OPTIONS.LINK),
+        active: false,
+        command: () => setIsAddLinkActive(not),
+        optionName: EDITOR_OPTIONS.LINK,
+        tooltip: tooltips.link ?? t("neetoEditor.menu.link"),
+      },
+      {
+        icon: TextCross,
+        isEnabled: options.includes(EDITOR_OPTIONS.STRIKETHROUGH),
+        command: editor.chain().focus().toggleStrike().run,
+        active: editor.isActive(EDITOR_OPTIONS.STRIKETHROUGH),
+        optionName: EDITOR_OPTIONS.STRIKETHROUGH,
+        tooltip: tooltips.strike ?? t("neetoEditor.menu.strike"),
+      },
+      {
+        icon: Highlight,
+        isEnabled: options.includes(EDITOR_OPTIONS.HIGHLIGHT),
+        command: editor.chain().focus().toggleHighlight().run,
+        active: editor.isActive(EDITOR_OPTIONS.HIGHLIGHT),
+        optionName: EDITOR_OPTIONS.HIGHLIGHT,
+        tooltip: tooltips.highlight ?? t("neetoEditor.menu.highlight"),
+      },
+    ],
+    list: [
+      {
+        icon: ListDot,
+        command: editor.chain().focus().toggleBulletList().run,
+        active: editor.isActive("bulletList"),
+        isEnabled: options.includes(EDITOR_OPTIONS.LIST_BULLETS),
+        optionName: EDITOR_OPTIONS.LIST_BULLETS,
+        highlight: true,
+        tooltip: tooltips.bulletList ?? t("neetoEditor.menu.bulletedList"),
+      },
+      {
+        icon: ListNumber,
+        command: editor.chain().focus().toggleOrderedList().run,
+        active: editor.isActive("orderedList"),
+        isEnabled: options.includes(EDITOR_OPTIONS.LIST_ORDERED),
+        optionName: EDITOR_OPTIONS.LIST_ORDERED,
+        highlight: true,
+        tooltip: tooltips.orderedList ?? t("neetoEditor.menu.orderedList"),
+      },
+    ],
+    block: [
+      {
+        icon: Quote,
+        command: editor.chain().focus().toggleBlockquote().run,
+        active: editor.isActive("blockquote"),
+        isEnabled: options.includes(EDITOR_OPTIONS.BLOCKQUOTE),
+        optionName: EDITOR_OPTIONS.BLOCKQUOTE,
+        highlight: true,
+        tooltip: tooltips.blockQuote ?? t("neetoEditor.menu.blockQuote"),
+      },
+      {
+        icon: Code,
+        command: editor.chain().focus().toggleCode().run,
+        active: editor.isActive(EDITOR_OPTIONS.CODE),
+        isEnabled: options.includes(EDITOR_OPTIONS.CODE),
+        optionName: EDITOR_OPTIONS.CODE,
+        tooltip: tooltips.code ?? t("neetoEditor.menu.code"),
+      },
+      {
+        icon: CodeBlock,
+        command: editor.chain().focus().toggleCodeBlock().run,
+        active: editor.isActive("codeBlock"),
+        isEnabled: options.includes(EDITOR_OPTIONS.CODE_BLOCK),
+        optionName: EDITOR_OPTIONS.CODE_BLOCK,
+        tooltip: tooltips.codeBlock ?? t("neetoEditor.menu.codeBlock"),
+      },
+    ],
+    misc: [
+      {
+        icon: Attachment,
+        command: attachmentProps?.handleUploadAttachments,
+        disabled: attachmentProps?.isDisabled,
+        active: false,
+        optionName: "attachments",
+        tooltip: tooltips.attachments || t("neetoEditor.menu.attachments"),
+      },
+      {
+        Icon: ImageUpload,
+        command: () => setMediaUploader(assoc("image", true)),
+        optionName: "image-upload",
+        tooltip: tooltips.imageUpload || t("neetoEditor.menu.imageUpload"),
+      },
+      {
+        Icon: Video,
+        command: () => setMediaUploader(assoc("video", true)),
+        optionName: "video-upload",
+        tooltip: tooltips.videoUpload || t("neetoEditor.menu.videoUpload"),
+      },
+      {
+        Icon: MediaVideo,
+        command: () => setIsEmbedModalOpen(true),
+        optionName: "video-embed",
+        tooltip: tooltips.videoEmbed || t("neetoEditor.menu.videoEmbed"),
+      },
+    ],
+  };
+};
 
 export const buildMenuOptions = ({
   tooltips,
