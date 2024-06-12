0b9df1c1c0299773ec81d11a763281b35d2ea8f6
Deepak Jose <deepakjosp@gmail.com>
2024-06-12 12:37:10 +0530

    Added a function to recursively upload files in the store to rails active storage.

--- src/hooks/useFileUploader.js
+++ src/hooks/useFileUploader.js
@@ -17,6 +17,51 @@ const useFileUploader = () => {
     updateFileUploadProgress(file.id, progress);
   };
 
+  const uploadFile = async file => {
+
+    const upload = new DirectUpload({
+      file: file.file,
+      url: "http://spinkart.lvh.me:9005/api/direct_uploads",
+      progress: xhr => handleUploadProgress(xhr, file),
+    });
+
+    try {
+      const blob = await upload.create();
+      await updateFileStatus(file.id, "uploaded");
+
+      return file;
+    } catch {
+      await updateFileStatus(file.id, "uploaded-error");
+    }
+  };
+
+  const uploadFiles = async () => {
+    if (isUploading) return [];
+
+    const handleUploadFiles = async () => {
+      const queuedFile = getNextQueuedFile();
+
+      if (isNotPresent(queuedFile)) {
+        setUploading(false);
+
+        return [];
+      }
+
+      setUploading(true);
+      // eslint-disable-next-line @bigbinary/neeto/combine-multiple-independent-awaits
+      const uploadedFile = await uploadFile(queuedFile);
+
+      const remainingUploadedFiles = await handleUploadFiles();
+
+      clearQueue();
+
+      return [uploadedFile, ...remainingUploadedFiles];
+    };
+
+    return handleUploadFiles();
+  };
+
+  return { addFiles, uploadFiles };
 };
 
 export default useFileUploader;
