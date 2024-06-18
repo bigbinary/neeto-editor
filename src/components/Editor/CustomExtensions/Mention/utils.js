4554597f06592331e7012854b34e63bcd298d660
Deepak Jose <deepakjosp@gmail.com>
2024-06-18 21:33:34 +0530

    Trimmed the mentions list query.

--- src/components/Editor/CustomExtensions/Mention/utils.js
+++ src/components/Editor/CustomExtensions/Mention/utils.js
@@ -1,13 +1,13 @@
 export const createMentionSuggestions =
   (mentions = []) =>
-  ({ query }) => {
+  ({ query = "" }) => {
     if (!query) {
       return mentions.slice(0, 10);
     }
 
     return mentions
       .filter(mention =>
-        mention.name.toLowerCase().includes(query.toLowerCase())
+        mention.name.toLowerCase().includes(query.trim().toLowerCase())
       )
       .slice(0, 10);
   };
