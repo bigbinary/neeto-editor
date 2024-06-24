38e5354275a5ea5b27a41b8b5c3cd98511d30fb2
Abhay V Ashokan <abhayvashokan@gmail.com>
2024-06-24 09:36:04 +0530

    Revert "Replaces the usages of uppy for uploads with custom hook. (#1138)"
    This reverts commit 142c3924ee5d845debff94be2aa1979a5e716451.

--- src/components/Attachments/index.jsx
+++ src/components/Attachments/index.jsx
@@ -1,25 +1,25 @@
 import React, {
   useRef,
+  useEffect,
   useState,
   useImperativeHandle,
   lazy,
   Suspense,
-  useMemo,
 } from "react";
 
+import DropTarget from "@uppy/drop-target";
 import classnames from "classnames";
-import { isNotEmpty, isPresent, noop } from "neetocist";
+import { isPresent, removeById, noop } from "neetocist";
 import { Button, Toastr } from "neetoui";
-import { isEmpty } from "ramda";
+import { concat, isEmpty, isNil } from "ramda";
 import { useTranslation } from "react-i18next";
 
-import useDropFiles from "hooks/useDropFiles";
-import useFileUploader from "hooks/useFileUploader";
+import useUppyUploader from "hooks/useUppyUploader";
 import "src/styles/components/attachments.scss";
 
 import Attachment from "./Attachment";
 import AttachmentProgress from "./AttachmentProgress";
-import { buildFileUploadConfig } from "./utils";
+import { buildUppyConfig, handleDrop, selectFiles } from "./utils";
 
 const Preview = lazy(() => import("./Preview"));
 
@@ -38,34 +38,110 @@ const Attachments = (
 ) => {
   const { t } = useTranslation();
 
+  const [pendingAttachments, setPendingAttachments] = useState([]);
   const [selectedAttachment, setSelectedAttachment] = useState({});
   const [didFetchPreviewBundle, setDidFetchPreviewBundle] = useState(false);
 
   const attachmentInputRef = useRef(null);
 
-  const fileUploadConfig = useMemo(
-    () => buildFileUploadConfig(config),
-    [config]
-  );
+  const { uppy, isUploading } = useUppyUploader({
+    uppyConfig: buildUppyConfig(config),
+  });
 
-  const { addFiles, uploadFiles, queuedFiles, cancelUpload, isUploading } =
-    useFileUploader({
-      config: fileUploadConfig,
-      setIsUploadingOnHost: setIsUploading,
-      attachments,
+  setIsUploading(isUploading);
+
+  const handleAddFile = event => {
+    const files = selectFiles({
+      previousAttachmentsCount: attachments.length,
+      config,
+      files: event.target.files,
     });
 
-  const handleAddFile = async event => {
-    addFiles(event.target.files);
-    const uploadedFiles = await uploadFiles();
-    isNotEmpty(uploadedFiles) && onChange([...attachments, ...uploadedFiles]);
+    files.forEach(file => {
+      try {
+        uppy.addFile({
+          name: file.name,
+          type: file.type,
+          data: file,
+        });
+      } catch (error) {
+        if (error.message !== t("neetoEditor.error.onBeforeFileAddedReturn")) {
+          Toastr.error(t("neetoEditor.error.cannotAddFiles"));
+        }
+      }
+    });
+
+    afterAddingFiles();
+  };
+
+  const onDrop = () => {
+    handleDrop({
+      uppy,
+      config,
+      previousAttachmentsCount: attachments.length,
+    }) && afterAddingFiles();
+  };
+
+  const afterAddingFiles = () => {
+    const newlyAddedFiles = uppy.getFiles().map(file => ({
+      id: file.id,
+      filename: file.name,
+      signedId: "awaiting",
+      url: "",
+      progress: 0,
+    }));
+    if (isEmpty(newlyAddedFiles)) {
+      uppy.reset();
+
+      return;
+    }
+    setPendingAttachments(concat(newlyAddedFiles));
     attachmentInputRef.current.value = null;
+    handleUpload();
+  };
+
+  const handleUpload = async () => {
+    try {
+      const response = await uppy.upload();
+
+      if (isNil(response)) return;
+      const uploadedFiles = response.successful.map(file => ({
+        filename: file.name,
+        signedId: file.response.data?.signed_id || file.response.signed_id,
+        url: file.response.data?.blob_url || file.response.blob_url,
+        contentType:
+          file.response.data?.content_type || file.response.content_type,
+      }));
+
+      setPendingAttachments([]);
+      onChange([...attachments, ...uploadedFiles]);
+    } catch (error) {
+      Toastr.error(error);
+    } finally {
+      uppy.reset();
+    }
+  };
+
+  const handleUploadProgress = (file, progress) => {
+    setPendingAttachments(prevState =>
+      prevState.map(uploadingFile => ({
+        ...uploadingFile,
+        progress:
+          uploadingFile.filename !== file.name
+            ? uploadingFile.progress
+            : progress.progress,
+      }))
+    );
+  };
+
+  const removeUploadingFile = id => {
+    setPendingAttachments(prevState => removeById(id, prevState));
   };
 
   const handleUploadAttachments = () => attachmentInputRef.current.click();
 
   const handleFileInputClick = event => {
-    if (!(!isEmpty(attachments) && fileUploadConfig.maxNumberOfFiles === 1)) {
+    if (!(!isEmpty(attachments) && config.maxNumberOfFiles === 1)) {
       return;
     }
     event.preventDefault();
@@ -74,17 +150,20 @@ const Attachments = (
 
   useImperativeHandle(ref, () => ({ handleUploadAttachments }), []);
 
-  const handleFilesDrop = async files => {
-    addFiles(files);
-    const uploadedFiles = await uploadFiles();
-    isNotEmpty(uploadedFiles) && onChange([...attachments, ...uploadedFiles]);
-  };
+  useEffect(() => {
+    uppy.on("upload-progress", handleUploadProgress);
+    if (dragDropRef?.current) {
+      uppy.use(DropTarget, { target: dragDropRef.current, onDrop });
+    }
 
-  useDropFiles({
-    dropTargetRef: dragDropRef,
-    attachments,
-    onDrop: handleFilesDrop,
-  });
+    return () => {
+      const instance = uppy.getPlugin("DropTarget");
+      if (instance) {
+        uppy.removePlugin(instance);
+      }
+      uppy.off("upload-progress", handleUploadProgress);
+    };
+  }, [attachments]);
 
   return (
     <div className={classnames("ne-attachments", { [className]: className })}>
@@ -105,9 +184,9 @@ const Attachments = (
             }
           />
         ))}
-        {queuedFiles.map(attachment => (
+        {pendingAttachments.map(attachment => (
           <AttachmentProgress
-            {...{ attachment, cancelUpload }}
+            {...{ attachment, removeUploadingFile, uppy }}
             key={attachment.id}
           />
         ))}
@@ -125,8 +204,8 @@ const Attachments = (
           />
         )}
         <input
-          accept={fileUploadConfig.allowedFileTypes?.join(",")}
-          multiple={fileUploadConfig.maxNumberOfFiles !== 1}
+          accept={config.allowedFileTypes?.join(",")}
+          multiple={config.maxNumberOfFiles !== 1}
           ref={attachmentInputRef}
           type="file"
           onChange={handleAddFile}
--- src/components/Editor/MediaUploader/LocalUploader.jsx
+++ src/components/Editor/MediaUploader/LocalUploader.jsx
@@ -1,18 +1,18 @@
-import React, { useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
+import DropTarget from "@uppy/drop-target";
 import { ImageUpload } from "neetoicons";
-import { Typography } from "neetoui";
-import { isEmpty } from "ramda";
+import { Toastr, Typography } from "neetoui";
+import { concat, isEmpty, isNil } from "ramda";
 import { useTranslation } from "react-i18next";
 
-import useDropFiles from "hooks/useDropFiles";
-import useFileUploader from "hooks/useFileUploader";
+import useUppyUploader from "hooks/useUppyUploader";
 
 import {
   ALLOWED_IMAGE_TYPES,
   ALLOWED_VIDEO_TYPES,
-  DEFAULT_IMAGE_UPLOAD_CONFIG,
-  DEFAULT_VIDEO_UPLOAD_CONFIG,
+  DEFAULT_IMAGE_UPPY_CONFIG,
+  DEFAULT_VIDEO_UPPY_CONFIG,
 } from "./constants";
 import Progress from "./Progress";
 import { convertToFileSize } from "./utils";
@@ -25,39 +25,90 @@ const LocalUploader = ({
 }) => {
   const { t } = useTranslation();
 
-  const uploadConfig = isImage
-    ? DEFAULT_IMAGE_UPLOAD_CONFIG
-    : DEFAULT_VIDEO_UPLOAD_CONFIG;
+  const [pendingUploads, setPendingUploads] = useState([]);
+
+  const uppyConfig = useMemo(
+    () => (isImage ? DEFAULT_IMAGE_UPPY_CONFIG : DEFAULT_VIDEO_UPPY_CONFIG),
+    []
+  );
 
   const fileInputRef = useRef(null);
   const dropTargetRef = useRef(null);
 
-  const { addFiles, uploadFiles, queuedFiles, cancelUpload, isUploading } =
-    useFileUploader({
-      config: uploadConfig,
-      setIsUploadingOnHost: setIsUploading,
-    });
-
-  const handleFilesDrop = async files => {
-    addFiles(files);
-    const uploadedFiles = await uploadFiles();
-    uploadedFiles.forEach(insertMediaToEditor);
-    onClose();
-  };
-
-  useDropFiles({ dropTargetRef, onDrop: handleFilesDrop });
+  const { uppy, isUploading } = useUppyUploader({ uppyConfig });
 
   setIsUploading(isUploading);
 
-  const handleAddFile = async ({ target: { files } }) => {
-    addFiles(files);
-    const uploadedFiles = await uploadFiles();
-    uploadedFiles.forEach(insertMediaToEditor);
-    onClose();
+  const handleAddFile = ({ target: { files } }) => {
+    Array.from(files).forEach(file => {
+      try {
+        uppy.addFile({
+          name: file.name,
+          type: file.type,
+          data: file,
+        });
+      } catch (error) {
+        if (error.message !== t("neetoEditor.error.onBeforeFileAddedReturn")) {
+          Toastr.error(t("neetoEditor.error.cannotAddFiles"));
+        }
+      }
+    });
+
+    afterAddingFiles();
   };
 
-  return !isEmpty(queuedFiles) || isUploading ? (
-    <Progress {...{ cancelUpload, queuedFiles }} />
+  const afterAddingFiles = () => {
+    const newlyAddedFiles = uppy.getFiles().map(file => ({
+      id: file.id,
+      filename: file.name,
+      signedId: "awaiting",
+      url: "",
+      progress: 0,
+    }));
+    if (isEmpty(newlyAddedFiles)) {
+      uppy.reset();
+
+      return;
+    }
+    setPendingUploads(concat(newlyAddedFiles));
+    handleUpload();
+  };
+
+  const handleUpload = async () => {
+    try {
+      const response = await uppy.upload();
+
+      if (isNil(response)) return;
+      const uploadedFiles = response.successful.map(file => ({
+        filename: file.name,
+        signedId: file.response.data?.signed_id || file.response.signed_id,
+        url: file.response.data?.blob_url || file.response.blob_url,
+      }));
+
+      setPendingUploads([]);
+      uploadedFiles.forEach(insertMediaToEditor);
+    } catch (error) {
+      Toastr.error(error);
+    } finally {
+      uppy.reset();
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    uppy.use(DropTarget, {
+      target: dropTargetRef?.current,
+      onDrop: afterAddingFiles,
+    });
+
+    return () => {
+      const instance = uppy.getPlugin("DropTarget");
+      instance && uppy.removePlugin(instance);
+    };
+  }, []);
+
+  return !isEmpty(pendingUploads) || isUploading ? (
+    <Progress {...{ pendingUploads, setPendingUploads, uppy }} />
   ) : (
     <div
       className="ne-media-uploader__dnd"
@@ -85,7 +136,7 @@ const LocalUploader = ({
       </Typography>
       <Typography style="body3">
         {t("neetoEditor.localUploader.maxFileSize", {
-          entity: convertToFileSize(uploadConfig.restrictions.maxFileSize),
+          entity: convertToFileSize(uppyConfig.restrictions.maxFileSize),
         })}
       </Typography>
     </div>
