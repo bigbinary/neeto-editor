f3d3c6ffe19da8925e30839e7ee7df7e6d340e09
Deepak Jose <deepakjosp@gmail.com>
2024-07-11 11:58:35 +0530

    Re-organized the elements in the file upload progress component and added the file icon.

--- src/components/Editor/MediaUploader/Progress.jsx
+++ src/components/Editor/MediaUploader/Progress.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { withT } from "neetocommons/react-utils";
 import { Close } from "neetoicons";
-import { Button, Typography } from "neetoui";
+
+import FileIcon from "../../Common/FileIcon";
 
 const Progress = withT(({ t, queuedFiles, cancelUpload }) => (
   <div className="ne-media-uploader__wrapper">
     {queuedFiles.map(({ id, filename, progress }) => (
       <div className="ne-media-uploader__media" key={id}>
-        <div className="ne-media-uploader__media__info">
-          <Typography style="body2">{filename}</Typography>
+        <div className="ne-media-uploader__media__cancel-button-wrapper">
           <Button
             data-cy="neeto-editor-image-upload-cancel-button"
             icon={Close}
@@ -18,20 +18,22 @@ const Progress = withT(({ t, queuedFiles, cancelUpload }) => (
             onClick={() => cancelUpload(id)}
           />
         </div>
-        {progress !== 100 ? (
+        <FileIcon
+          className="ne-media-uploader__media__info__icon"
+          fileName={filename}
+        />
+        <div className="ne-media-uploader__media__info">
+          <Typography style="nano">
+            {progress === 100 && t("neetoEditor.localUploader.completed")}
+            {progress !== 100 && <>{progress}%</>}
+          </Typography>
           <div className="ne-media-uploader__media__progress">
             <div
               className="ne-media-uploader__media__progress-bar"
               style={{ width: `${progress}%` }}
-            >
-              {progress}%
-            </div>
+            />
           </div>
-        ) : (
-          <Typography style="body3">
-            {t("neetoEditor.localUploader.completed")}
-          </Typography>
-        )}
+          <Typography style="body3">{filename}</Typography>
       </div>
     ))}
   </div>
