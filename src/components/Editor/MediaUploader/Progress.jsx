563b11f851732f61b35d0063a9489d7042dc41a9
Deepak Jose <deepakjosp@gmail.com>
2024-07-11 18:23:37 +0530

    Improvises the UI of upload progress modal. (#1178)
    * Moved fileIcon to commons folder to re-use in the upload modal.
    
    * Re-organized the elements in the file upload progress component and added the file icon.
    
    * Reduced the icon size of the cancel button.
    
    * Added a tooltip around the filename to make it easy to read.
    
    * Set the initial value to the progress and removed the padding of the progress bar.
    
    * Removed the height and instead set the gap of the flex container of the media progress component.
    
    * Added the missing type in the progress bar border radius variable.
    
    * Removed the unused style properties from the progress bar since the progress text is moved out.
    
    * Button fix
    
    ---------
    
    Co-authored-by: Praveen Murali <itspraveenmurali@gmail.com>

--- src/components/Editor/MediaUploader/Progress.jsx
+++ src/components/Editor/MediaUploader/Progress.jsx
@@ -2,36 +2,43 @@ import React from "react";
 
 import { withT } from "neetocommons/react-utils";
 import { Close } from "neetoicons";
-import { Button, Typography } from "neetoui";
+import { Button, Tooltip, Typography } from "neetoui";
+
+import FileIcon from "../../Common/FileIcon";
 
 const Progress = withT(({ t, queuedFiles, cancelUpload }) => (
   <div className="ne-media-uploader__wrapper">
-    {queuedFiles.map(({ id, filename, progress }) => (
+    {queuedFiles.map(({ id, filename, progress = 0 }) => (
       <div className="ne-media-uploader__media" key={id}>
-        <div className="ne-media-uploader__media__info">
-          <Typography style="body2">{filename}</Typography>
+        <div className="ne-media-uploader__media__cancel-button-wrapper">
           <Button
             data-cy="neeto-editor-image-upload-cancel-button"
             icon={Close}
+            iconSize={18}
             size="small"
             style="text"
             onClick={() => cancelUpload(id)}
           />
         </div>
-        {progress !== 100 ? (
+        <FileIcon
+          className="ne-media-uploader__media__info__icon"
+          fileName={filename}
+        />
+        <div className="ne-media-uploader__media__info">
+          <Typography style="nano">
+            {progress === 100 && t("neetoEditor.localUploader.completed")}
+            {progress !== 100 && <>{progress}%</>}
+          </Typography>
           <div className="ne-media-uploader__media__progress">
             <div
               className="ne-media-uploader__media__progress-bar"
               style={{ width: `${progress}%` }}
-            >
-              {progress}%
-            </div>
+            />
           </div>
-        ) : (
-          <Typography style="body3">
-            {t("neetoEditor.localUploader.completed")}
-          </Typography>
-        )}
+          <Tooltip content={filename} position="top">
+            <Typography style="body3">{filename}</Typography>
+          </Tooltip>
+        </div>
       </div>
     ))}
   </div>
