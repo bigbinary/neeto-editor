3067f79b381fd4ba85ce21c300eaa90bcc7f0a76
Deepak Jose <deepakjosp@gmail.com>
2024-07-16 12:52:56 +0530

    Replaced the dummy icons from the table action menu with actual icons from neeto-icons.

--- src/components/Editor/CustomExtensions/Table/utils.js
+++ src/components/Editor/CustomExtensions/Table/utils.js
@@ -1,48 +1,48 @@
 import { t } from "i18next";
 import {
-  TextBold,
-  TextItalic,
-  Delete,
-  TextCross,
-  Highlight,
-  Merge,
-  Header,
+  DeleteRow,
+  DeleteColumn,
+  DeleteTable,
+  InsertRow,
+  InsertColumn,
+  MergeSplit,
+  ToggleHeaderRow,
 } from "neetoicons";
 
 export const tableActions = ({ editor }) => [
   {
     label: t("neetoEditor.table.deleteRow"),
     command: () => editor.chain().focus().deleteRow().run(),
-    icon: TextBold,
+    icon: DeleteRow,
   },
   {
     label: t("neetoEditor.table.deleteColumn"),
     command: () => editor.chain().focus().deleteColumn().run(),
-    icon: TextItalic,
+    icon: DeleteColumn,
   },
   {
     label: t("neetoEditor.table.delete"),
     command: () => editor.commands.deleteTable(),
-    icon: Delete,
+    icon: DeleteTable,
   },
   {
     label: t("neetoEditor.table.insertRow"),
     command: () => editor.commands.addRowAfter(),
-    icon: TextCross,
+    icon: InsertRow,
   },
   {
     label: t("neetoEditor.table.insertColumn"),
     command: () => editor.commands.addColumnAfter(),
-    icon: Highlight,
+    icon: InsertColumn,
   },
   {
     label: t("neetoEditor.table.mergeSplit"),
     command: () => editor.chain().focus().mergeOrSplit().run(),
-    icon: Merge,
+    icon: MergeSplit,
   },
   {
     label: t("neetoEditor.table.toggleHeaderRow"),
     command: () => editor.chain().focus().toggleHeaderRow().run(),
-    icon: Header,
+    icon: ToggleHeaderRow,
   },
 ];
