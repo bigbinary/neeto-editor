5dd6af1c78155d06399a5febeb63e79136de7c94
Deepak Jose <deepakjosp@gmail.com>
2024-07-17 13:12:27 +0530

    Fixed the order of the menu items.

--- src/components/Editor/CustomExtensions/Table/utils.js
+++ src/components/Editor/CustomExtensions/Table/utils.js
@@ -11,29 +11,28 @@ import {
 
 export const tableActions = ({ editor }) => [
   {
-    label: t("neetoEditor.table.deleteRow"),
-    command: () => editor.chain().focus().deleteRow().run(),
-    icon: DeleteRow,
-  },
-  {
-    label: t("neetoEditor.table.deleteColumn"),
-    command: () => editor.chain().focus().deleteColumn().run(),
-    icon: DeleteColumn,
-  },
-  {
-    label: t("neetoEditor.table.delete"),
-    command: () => editor.commands.deleteTable(),
-    icon: DeleteTable,
-  },
-  {
     label: t("neetoEditor.table.insertRow"),
     command: () => editor.commands.addRowAfter(),
     icon: InsertRow,
+    isVisible: true,
   },
   {
     label: t("neetoEditor.table.insertColumn"),
     command: () => editor.commands.addColumnAfter(),
     icon: InsertColumn,
+    isVisible: true,
+  },
+  {
+    label: t("neetoEditor.table.deleteRow"),
+    command: () => editor.chain().focus().deleteRow().run(),
+    icon: DeleteRow,
+    isVisible: true,
+  },
+  {
+    label: t("neetoEditor.table.deleteColumn"),
+    command: () => editor.chain().focus().deleteColumn().run(),
+    icon: DeleteColumn,
+    isVisible: true,
   },
   {
     label: t("neetoEditor.table.mergeSplit"),
@@ -46,5 +45,10 @@ export const tableActions = ({ editor }) => [
     icon: ToggleHeaderRow,
     isVisible: editor.isActive("tableHeader"),
   },
+  {
+    label: t("neetoEditor.table.delete"),
+    command: () => editor.commands.deleteTable(),
+    icon: DeleteTable,
+    isVisible: true,
   },
 ];
