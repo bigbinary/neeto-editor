e05acdc3aecc2b36a6b78fc4caa8285b4be5c1d6
Deepak Jose <deepakjosp@gmail.com>
2024-08-01 15:16:46 +0530

    Modified the render part to return the NodeViewWrapper only if it's in the view port and an empty div it's out of the view port.

--- src/components/Editor/CustomExtensions/CodeBlock/CodeBlockComponent.jsx
+++ src/components/Editor/CustomExtensions/CodeBlock/CodeBlockComponent.jsx
@@ -52,50 +52,59 @@ const CodeBlockComponent = ({ node, editor, updateAttributes }) => {
   }, []);
 
   return (
-    <NodeViewWrapper data-cy="neeto-editor-code-block">
-      <pre>
-        <div className="neeto-editor-codeblock-options" contentEditable={false}>
-          <Dropdown
-            appendTo={() => document.body}
-            buttonSize="small"
-            buttonStyle="secondary"
-            icon={Down}
-            label={node.attrs?.language || t("neetoEditor.common.auto")}
-            strategy="fixed"
-            zIndex={99999}
-          >
-            <Input
-              autoFocus
-              className="neeto-editor-codeblock-options__input"
-              placeholder={t("neetoEditor.placeholders.searchLanguages")}
-              size="small"
-              value={keyword}
-              onChange={e => setKeyword(e.target.value)}
-              onClick={e => {
-                e.preventDefault();
-                e.stopPropagation();
-              }}
-            />
-            <Menu className="neeto-editor-codeblock-options__menu">
-              {filteredAndSortedLanguageList.map(language => (
-                <MenuItem.Button
-                  key={language}
-                  onClick={() => handleLanguageSelect(language)}
+    <div {...{ ref }}>
+      {isVisible && (
+        <NodeViewWrapper data-cy="neeto-editor-code-block">
+          <div>
+            <pre ref={handleContentMount}>
+              <div
+                className="neeto-editor-codeblock-options"
+                contentEditable={false}
+              >
+                <Dropdown
+                  appendTo={() => document.body}
+                  buttonSize="small"
+                  buttonStyle="secondary"
+                  icon={Down}
+                  label={node.attrs?.language || t("neetoEditor.common.auto")}
+                  strategy="fixed"
+                  zIndex={99999}
                 >
-                  {language || t("neetoEditor.common.auto")}
-                </MenuItem.Button>
-              ))}
-            </Menu>
-          </Dropdown>
-          <CopyToClipboardButton
-            size="small"
-            style="secondary"
-            value={node?.content?.content[0]?.text}
-          />
-        </div>
-        <NodeViewContent as="code" />
-      </pre>
-    </NodeViewWrapper>
+                  <Input
+                    autoFocus
+                    className="neeto-editor-codeblock-options__input"
+                    placeholder={t("neetoEditor.placeholders.searchLanguages")}
+                    size="small"
+                    value={keyword}
+                    onChange={e => setKeyword(e.target.value)}
+                    onClick={e => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                  />
+                  <Menu className="neeto-editor-codeblock-options__menu">
+                    {filteredAndSortedLanguageList.map(language => (
+                      <MenuItem.Button
+                        key={language}
+                        onClick={() => handleLanguageSelect(language)}
+                      >
+                        {language || t("neetoEditor.common.auto")}
+                      </MenuItem.Button>
+                    ))}
+                  </Menu>
+                </Dropdown>
+                <CopyToClipboardButton
+                  size="small"
+                  style="secondary"
+                  value={node?.content?.content[0]?.text}
+                />
+              </div>
+              <NodeViewContent as="code" />
+            </pre>
+          </div>
+        </NodeViewWrapper>
+      )}
+    </div>
   );
 };
 
