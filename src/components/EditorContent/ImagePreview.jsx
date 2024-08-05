7dcd7373ba6d7a16a21fc28f5232ee57fc31d43a
Deepak Jose <deepakjosp@gmail.com>
2024-08-05 08:55:01 +0530

    Audit and fixes the image preview component of EditorContent. (#1197)
    * Removed the pointer events check and added event.preventDefault to prevent event propagation.
    
    * Fixed image preview style issues.
    
    * Refactored the query selector used to select figures and code blocks.
    
    * Refactored the image preview component and fixed the style issues.
    
    * Refactored the image preview component to show the caption if present.
    
    * Added translation for the alt text.
    
    * Fixed the spinner style issues and moved the styles to the editor content scss file.
    
    * Changes in style build outputs.
    
    * Fixed the image preview alignment issue in the editor preview.

--- src/components/EditorContent/ImagePreview.jsx
+++ src/components/EditorContent/ImagePreview.jsx
@@ -1,49 +1,65 @@
 import React, { useEffect, useRef, useState } from "react";
 
+import classnames from "classnames";
 import { useOnClickOutside } from "neetocommons/react-utils";
 import { Close } from "neetoicons";
 import { Button, Spinner } from "neetoui";
 import { createPortal } from "react-dom";
+import { useTranslation } from "react-i18next";
 
-const ImagePreview = ({ imagePreviewUrl, setImagePreviewUrl }) => {
+const ImagePreview = ({ imagePreviewDetails, setImagePreviewDetails }) => {
   const [isLoading, setIsLoading] = useState(true);
+
+  const { t } = useTranslation();
 
   const imagePreviewRef = useRef(null);
 
-  useOnClickOutside(imagePreviewRef, () => setImagePreviewUrl(null), {
+  useOnClickOutside(imagePreviewRef, () => setImagePreviewDetails(null), {
     enabled: true,
   });
 
   useEffect(() => {
     document.addEventListener(
       "keydown",
-      e => e.key === "Escape" && setImagePreviewUrl(null)
+      e => e.key === "Escape" && setImagePreviewDetails(null)
     );
 
     return () =>
       document.removeEventListener(
         "keydown",
-        e => e.key === "Escape" && setImagePreviewUrl(null)
+        e => e.key === "Escape" && setImagePreviewDetails(null)
       );
   }, []);
 
   return createPortal(
-    <div className="image-preview-wrapper">
-      <div className="close-button">
-        <Button
-          icon={Close}
-          style="secondary"
-          onClick={() => setImagePreviewUrl(null)}
+    <div className="ne-image-preview-wrapper">
+      {isLoading && <Spinner className="ne-image-preview-wrapper__spinner" />}
+      {!isLoading && (
+        <div className="close-button">
+          <Button
+            icon={Close}
+            style="secondary"
+            onClick={() => setImagePreviewDetails(null)}
+          />
+        </div>
+      )}
+      <div
+        className={classnames("ne-image-preview", {
+          "image-loaded": !isLoading,
+        })}
+      >
+        <img
+          alt={t("neetoEditor.editorContent.imagePreviewAltText")}
+          ref={imagePreviewRef}
+          src={imagePreviewDetails.src}
+          onLoad={() => setIsLoading(false)}
         />
+        {imagePreviewDetails && (
+          <p className="ne-image-preview__caption">
+            {imagePreviewDetails.caption}
+          </p>
+        )}
       </div>
-      {isLoading && <Spinner className="spinner" />}
-      <img
-        alt="Image Preview"
-        className="image-preview"
-        ref={imagePreviewRef}
-        src={imagePreviewUrl}
-        onLoad={() => setIsLoading(false)}
-      />
     </div>,
     document.body
   );
