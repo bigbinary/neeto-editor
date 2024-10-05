d14b9f3b28fdeae4ade3999507e78449e3939651
Deepak Jose <deepakjosp@gmail.com>
2024-10-05 16:05:45 +0530

    Improves the list item backspace behaviour. (#1232)
    * Created custom extension for the bulletted list to custom handle the backspace behaviour.
    
    * Fixed the empty node backspace pushing in to an infinite loop.
    
    * Fixed the edge case of not able delete sub list node when there was nodes on top an below.

---
+++ src/components/Editor/CustomExtensions/BulletList/ExtensionConfig.js
@@ -0,0 +1,52 @@
+import BulletList from "@tiptap/extension-bullet-list";
+
+function isListNode(node) {
+  return node.type.name === "bulletList" || node.type.name === "orderedList";
+}
+
+export default BulletList.extend({
+  addKeyboardShortcuts() {
+    return {
+      Backspace: () =>
+        this.editor.commands.command(({ state, commands }) => {
+          const { $from, empty } = state.selection;
+
+          if (!empty) return false;
+
+          const node = $from.node();
+          if (node.type.name !== "paragraph" || node.content.size !== 0) {
+            return false;
+          }
+
+          const $listItem = $from.node($from.depth - 1);
+          if ($listItem.type.name !== "listItem") {
+            return commands.deleteCurrentNode();
+          }
+
+          const currentIndex = $from.index($from.depth - 1);
+
+          // lift the block if the the list item is empty.
+          if (currentIndex === $listItem.childCount - 1) {
+            return commands.liftEmptyBlock();
+          }
+
+          const nextNode = $listItem.child(currentIndex + 1);
+          if (!isListNode(nextNode)) {
+            return commands.deleteCurrentNode();
+          }
+
+          if (currentIndex === 0) {
+            return commands.joinBackward();
+          }
+
+          // Delete the empty paragraph node between the two lists.
+          const prevNode = $listItem.child(currentIndex - 1);
+          if (isListNode(prevNode)) {
+            return commands.deleteCurrentNode();
+          }
+
+          return commands.joinBackward();
+        }),
+    };
+  },
+});
