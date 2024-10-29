1695e0b81ef17fabdaa241bb321eba6bb644e443
Deepak Jose <deepakjosp@gmail.com>
2024-10-29 09:31:14 +0530

    Removed the unused file download api file.

--- src/apis/file_download.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const newAxiosInstance = axios.create();
-
-const getFile = url => newAxiosInstance.get(url, { responseType: "blob" });
-
-const fileDownloadApi = { getFile };
-
-export default fileDownloadApi;
