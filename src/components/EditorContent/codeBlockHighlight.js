672ff42fc9f32b669ad8b0bcbd31aaa5b7a370a0
Deepak Jose <deepakjosp@gmail.com>
2024-11-22 18:23:34 +0530

    Updated the codeblock highlight script to inject a copy button to the codeblock.

--- src/components/EditorContent/codeBlockHighlight.js
+++ src/components/EditorContent/codeBlockHighlight.js
@@ -4,22 +4,65 @@ import hljs from "highlight.js/lib/common";
 const highlightLinesScriptCDNPath =
   "//cdn.jsdelivr.net/gh/TRSasasusu/highlightjs-highlight-lines.js@1.2.0/highlightjs-highlight-lines.min.js";
 
-function applyLineHighlighting(codeElement) {
+const copyToClipboard = text => {
+  try {
+    if (navigator.clipboard && window.isSecureContext) {
+      navigator.clipboard.writeText(text);
+    } else {
+      const textArea = document.createElement("textarea");
+      textArea.value = text;
+
+      textArea.style.top = "0";
+      textArea.style.left = "0";
+      textArea.style.position = "fixed";
+
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+
+const addCopyToClipboardButton = codeElement => {
+  const copyButton = document.createElement("button");
+  copyButton.setAttribute(
+    "class",
+    "text-primary-grey absolute right-3 top-3 text-xs copy-button"
+  );
+  copyButton.innerText = "Copy";
+
+  copyButton.addEventListener("click", () => {
+    copyToClipboard(codeElement.innerText);
+    copyButton.innerText = "Copied";
+    setTimeout(() => {
+      copyButton.innerText = "Copy";
+    }, 2500);
+  });
+
+  codeElement.parentNode.appendChild(copyButton);
+};
+
+function applyCodeblockDecorations(codeElement) {
   hljs.highlightElement(codeElement);
+  addCopyToClipboardButton(codeElement);
   const preElement = codeElement.closest("pre");
 
   const highlightedLines = preElement.getAttribute("data-highlighted-lines");
-  if (highlightedLines) {
-    const linesToHighlight = highlightedLines.split(",").map(Number);
-    const highlightLinesOptions = linesToHighlight
-      .filter(line => line > 0)
-      .map(line => ({
-        start: line,
-        end: line,
-        color: "rgba(255, 255, 0, 0.2)",
-      }));
-    hljs.highlightLinesElement(codeElement, highlightLinesOptions);
-  }
+  const linesToHighlight = highlightedLines?.split(",")?.map(Number) ?? [0];
+  const highlightLinesOptions = linesToHighlight
+    .filter(line => line > 0)
+    .map(line => ({
+      start: line,
+      end: line,
+      color: "rgba(255, 255, 0, 0.2)",
+    }));
+  hljs.highlightLinesElement(codeElement, highlightLinesOptions);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -28,6 +71,6 @@ document.addEventListener("DOMContentLoaded", () => {
   script.async = true;
   document.head.appendChild(script);
   script.addEventListener("load", () => {
-    document.querySelectorAll("pre code").forEach(applyLineHighlighting);
+    document.querySelectorAll("pre code").forEach(applyCodeblockDecorations);
   });
 });
