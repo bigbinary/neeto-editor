958650d50877e0306298bcd203232bc154d878ba
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-01-20 18:19:38 +0530

    Added a publish script to publish assets to the S3 bucket

---
+++ scripts/publish.js
@@ -0,0 +1,35 @@
+const Uploader = require("@bigbinary/s3-uploader");
+const dotEnv = require("dotenv");
+
+dotEnv.config({ path: ".env" });
+
+const getS3UploaderConfig = () => ({
+  bucket: "neeto-editor-production",
+  destination: "neeto-editor",
+  distribution: "TODO",
+  distributionPath: "/neeto-editor*",
+  fileProperties: {
+    "*": { CacheControl: "no-cache" },
+  },
+});
+
+const uploadFiles = async () => {
+  const uploaderConfig = getS3UploaderConfig();
+  const uploader = new Uploader(uploaderConfig);
+
+  uploader.addFile("dist/editor-content.min.css");
+  uploader.addFile("dist/editor-output-pdf-email.css");
+  uploader.addFile("dist/codeblockHighlight.js");
+  uploader.addFile("dist/headerLinks.js");
+  uploader.addFile("dist/editorUtils.js");
+
+  try {
+    console.log("Starting upload...");
+    await uploader.upload();
+    console.log("Upload completed successfully.");
+  } catch (error) {
+    console.error("Upload failed:", error);
+  }
+};
+
+uploadFiles();
