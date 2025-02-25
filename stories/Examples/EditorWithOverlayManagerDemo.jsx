9e3b964b9f1dac92b209dc0527d81259eb0bee78
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-02-25 21:52:31 +0530

    [TEMP] Update OverlayManager story for demo purposes

--- stories/Examples/EditorWithOverlayManagerDemo.jsx
+++ stories/Examples/EditorWithOverlayManagerDemo.jsx
@@ -2,27 +2,43 @@ import React, { useRef, useState } from "react";
 
 import { Button, Pane, Typography } from "@bigbinary/neetoui";
 
-import { Editor } from "../../src";
+import { Editor, EditorContent } from "../../src";
 
 const EditorWithOverlayManager = () => {
-  const [showPane, setShowPane] = useState(false);
-  const initialFocusRef = useRef(null);
+  const [content, setContent] = useState(
+    `<pre data-highlighted-lines=""><code class="language-css">mark {
+  border-radius: var(--neeto-editor-rounded-sm);
+  padding: 0px;
+}
+
+[data-type="mention"] {
+  color: rgb(var(--neeto-editor-accent-800));
+}</code></pre>`
+  );
 
   return (
     <>
-      <Button label="Show Pane" onClick={() => setShowPane(true)} />
-      <Pane
-        initialFocusRef={initialFocusRef}
-        isOpen={showPane}
-        onClose={() => setShowPane(false)}
-      >
-        <Pane.Header>
-          <Typography style="h2" weight="semibold">
-            Typography
-          </Typography>
-        </Pane.Header>
-        <Editor ref={initialFocusRef} />
-      </Pane>
+      <Editor
+        initialValue={content}
+        onChange={setContent}
+        addons={[
+          "highlight",
+          "emoji",
+          "code-block",
+          "block-quote",
+          "image-upload",
+          "image-upload-unsplash",
+          "video-upload",
+          "video-embed",
+          "paste-unformatted",
+          "undo",
+          "redo",
+          "attachments",
+          "table",
+          "text-color",
+        ]}
+      />
+      <EditorContent content={content} />
     </>
   );
 };
