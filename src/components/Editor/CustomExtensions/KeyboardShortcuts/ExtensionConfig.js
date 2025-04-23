53c1be91f8043cf5168ee984af43c2a7af6601a4
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-04-23 20:31:15 +0530

    Revert "Added backspace keyboard shortcut to custom handle the issues in deleting the list item. (#1191)"
    This reverts commit 166d28333b23a137da13ccbd40f372942711105e.

--- src/components/Editor/CustomExtensions/KeyboardShortcuts/ExtensionConfig.js
+++ src/components/Editor/CustomExtensions/KeyboardShortcuts/ExtensionConfig.js
@@ -1,5 +1,4 @@
 import { Extension } from "@tiptap/core";
-import { liftTarget } from "@tiptap/pm/transform";
 
 const KeyboardShortcuts = ({ onSubmit, shortcuts, isBlockQuoteActive }) =>
   Extension.create({
@@ -21,40 +20,6 @@ const KeyboardShortcuts = ({ onSubmit, shortcuts, isBlockQuoteActive }) =>
 
           return false;
         },
-        // To fix the issue with backspace on the empty list item moving the focus to the block on top.
-        // https://github.com/ueberdosis/tiptap/issues/2829#issuecomment-1511064298
-        Backspace: () =>
-          this.editor.commands.command(({ tr }) => {
-            const { selection, doc } = tr;
-            const { $cursor } = selection;
-            const depth = $cursor?.depth;
-
-            if (
-              $cursor &&
-              depth >= 3 && // At least the structure is doc -> orderedList/bulletList -> listItem -> paragraph
-              $cursor.parent.type.name === "paragraph" && // The cursor is inside a paragraph.
-              $cursor.parentOffset === 0 && // The cursor is at the beginning of the paragraph.
-              $cursor.node(depth - 1).type.name === "listItem" && // The paragraph is inside a listItem.
-              $cursor.index(depth - 1) === 0 && // The paragraph is at the beginning of the listItem.
-              $cursor.index(depth - 2) === 0 // The listItem is at the beginning of the list.
-            ) {
-              const listItemNode = $cursor.node(depth - 1);
-              const listItemPos = $cursor.before(depth - 1);
-              const $contentBegin = doc.resolve(listItemPos + 1);
-              const $contentEnd = doc.resolve(
-                listItemPos + listItemNode.nodeSize - 1
-              );
-              const range = $contentBegin.blockRange($contentEnd);
-              const target = liftTarget(range);
-              if (target !== null) {
-                tr.lift(range, target);
-
-                return true;
-              }
-            }
-
-            return false;
-          }),
         ...shortcuts,
       };
     },
