65073303ddf8eaad67f33ee6d1b3af8b4b3473bb
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-06-20 16:04:59 +0530

    Repaced the emojis in callout menu with icons

--- src/components/Editor/Menu/Fixed/components/CalloutDropdown/CalloutIcon.jsx
+++ src/components/Editor/Menu/Fixed/components/CalloutDropdown/CalloutIcon.jsx
@@ -1,10 +1,12 @@
 import { Flag } from "neetoicons";
 
 const CalloutIcon = ({ currentType }) => {
-  if (currentType?.emoji) {
+  if (currentType?.icon) {
+    const Icon = currentType.icon;
+
     return (
       <span className="neeto-editor-callout-dropdown__current-emoji">
-        {currentType.emoji}
+        <Icon />
       </span>
     );
   }
