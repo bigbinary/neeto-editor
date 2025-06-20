9edd4a1d470526aaa72f5e6536b0bedfea586d18
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-06-20 16:14:17 +0530

    Replaced the rendered emoji in the NodeView with icons

--- src/components/Editor/CustomExtensions/Callout/CalloutComponent.jsx
+++ src/components/Editor/CustomExtensions/Callout/CalloutComponent.jsx
@@ -1,18 +1,24 @@
 import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
+import { findBy } from "neetocist";
 import { Close } from "neetoicons";
+import { Megaphone } from "neetoicons/misc";
 import { Button } from "neetoui";
 
+import { CALLOUT_TYPES } from "components/Editor/Menu/Fixed/components/CalloutDropdown/constants";
+
 const CalloutComponent = ({ node, deleteNode }) => {
-  const { type, emoji } = node.attrs;
+  const { type } = node.attrs;
+  const Icon = findBy({ type }, CALLOUT_TYPES)?.icon || Megaphone;
 
   return (
     <NodeViewWrapper
       className={`neeto-editor__callout neeto-editor__callout--${type} group`}
-      data-emoji={emoji}
-      data-type={type}
+      data-emoji={type}
     >
       <div className="callout-container">
-        <span className="callout-emoji">{emoji}</span>
+        <span className="callout-emoji">
+          <Icon />
+        </span>
         <NodeViewContent className="callout-content" />
         <Button
           className="callout-delete-button"
